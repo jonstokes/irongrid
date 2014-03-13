cdd0e3f9075fa3da2ff1d19d607a317de88e541c
Jon Stokes <jon@paperhatpress.com>
2014-03-13 14:04:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Irongrid::Application.config.secret_key_base = 'd3ceb4795b7ae5a6e222adc902e6dcf0009af73793667e866985e1244854bc90340d28199d1d4b35e274397e2eef21ea3393ebd5cc72cc799d22d02800433ec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Irongrid::Application.config.session_store :cookie_store, key: '_irongrid_session'
