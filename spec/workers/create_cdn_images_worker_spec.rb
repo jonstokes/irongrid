4f9723e8f016b3af714efee98c441158bfd6c197
Jon Stokes <jon@paperhatpress.com>
2014-03-27 17:57:18 -0500

    Cleaned up CreateCdnImage spec

--- spec/workers/create_cdn_images_worker_spec.rb
+++ spec/workers/create_cdn_images_worker_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'mocktra'
 require 'sidekiq/testing'
+require 'webmock/rspec'
 Sidekiq::Testing.fake!
 
 describe CreateCdnImagesWorker do
@@ -19,33 +20,9 @@ describe CreateCdnImagesWorker do
     image_source = "http://www.retailer.com/images/1.png"
     iq = ImageQueue.new(domain: @site.domain)
     iq.push image_source
-    CreateCdnImagesWorker.new.perform(domain: @site.domain)
+    worker = CreateCdnImagesWorker.new
+    worker.perform(domain: @site.domain)
+    WebMock.should have_requested(:get, "www.retailer.com/images/1.png")
     expect(CDN.has_image?(image_source)).to be_true
-  end
-
-  it "does not download an image that's already on the CDN" do
-    pending "Example"
-  end
-
-  it "resizes an image that's over 200x200 before uploading it to the CDN" do
-    pending "Example"
-  end
-
-  it "does not try to download a busted image more than three times over the life of a listing" do
-    pending "Example"
-    # Check listing.image_download_count
-    # If listing.image_download_count > 3 don't bother
-    # Else try to pull the listing
-    #   if the listing is there, then
-    #      d/l and resize and add to CDN and clear image_download_count
-    #   else increment the image_download_count
-    #
-  end
-  it "pulls an image url from the IS for a domain" do
-    pending "Example"
-  end
-
-  it "respects the site's rate limits" do
-    pending "Example"
   end
 end
