71b199dddcc52d483e07844586b1d1b10a5d06aa
Jon Stokes <jon@paperhatpress.com>
2014-03-31 13:04:20 -0500

    Added archive_all function to log_record

--- app/models/log_record.rb
+++ app/models/log_record.rb
@@ -6,4 +6,12 @@ class LogRecord < ActiveRecord::Base
   attr_accessible :data, :jid, :agent, :archived
 
   scope :active, -> { where(:archived => [nil, false]) }
+
+  def self.archive_all
+    db do
+      JobRecord.where(:archived => [false, nil]).each do |record |
+        record.update_attribute(:archived, true)
+      end
+    end
+  end
 end
