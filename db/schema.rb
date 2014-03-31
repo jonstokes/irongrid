dbdd87a666898ea05e9c0b3852c788761e2dfdfe
Jon Stokes <jon@paperhatpress.com>
2014-03-31 16:11:38 -0500

    Schema now reflects rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -106,8 +106,21 @@ ActiveRecord::Schema.define(version: 20140327132319) do
     t.integer  "number_of_rounds"
     t.integer  "grains"
     t.string   "manufacturer"
-    t.string   "model"
+    t.string   "caliber_category"
   end
+
+  create_table "rails_admin_histories", force: true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
   create_table "service_records", force: true do |t|
     t.string   "service"
@@ -138,5 +151,23 @@ ActiveRecord::Schema.define(version: 20140327132319) do
     t.integer  "read_interval"
     t.string   "commit_sha"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
