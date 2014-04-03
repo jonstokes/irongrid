5ab3a6e2c8cea48e56d8ff2c545d983d781cf4a4
Jon Stokes <jon@paperhatpress.com>
2014-04-03 16:08:28 -0500

    Added spec for DeleteEndedAuctionsWorker fixes

--- spec/workers/delete_ended_auctions_worker_spec.rb
+++ spec/workers/delete_ended_auctions_worker_spec.rb
@@ -15,4 +15,23 @@ describe DeleteEndedAuctionsWorker do
       Listing.find auctions.last.id
     }.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "does not fail when it encounters a listing that has already been deleted" do
+    site = create_site_from_repo "www.gunbroker.com"
+    5.times { FactoryGirl.create(:auction_listing) }
+    auctions = []
+    2.times { auctions << FactoryGirl.create(:auction_listing, :ended) }
+    DeleteEndedAuctionsWorker.new.perform(auctions)
+    expect(AuctionListing.all.count).to eq(5)
+    expect {
+      Listing.find auctions.first.id
+    }.to raise_error(ActiveRecord::RecordNotFound)
+    expect {
+      Listing.find auctions.last.id
+    }.to raise_error(ActiveRecord::RecordNotFound)
+    expect {
+      DeleteEndedAuctionsWorker.new.perform(auctions)
+    }.not_to raise_error
+  end
+
 end
