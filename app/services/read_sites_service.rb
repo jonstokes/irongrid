05fae5f35e26e1041dee4c0632f94626f9d45b1a
Jon Stokes <jon@paperhatpress.com>
2014-04-04 13:02:20 -0500

    Don't double-lock jobs

--- app/services/read_sites_service.rb
+++ app/services/read_sites_service.rb
@@ -8,25 +8,21 @@ class ReadSitesService < CoreService
   )
 
   def jobs
-    $mutex.synchronize {
-      Site.active.map do |site|
-        next unless should_add_job?(site)
-        next if ["RefreshLinksWorker", "CreateLinksWorker"].include?(site.read_with) && LinkQueue.new(domain: site.domain).any?
-        { klass: site.read_with, arguments: {domain: site.domain} }
-      end.compact
-    }
+    Site.active.map do |site|
+      next unless should_add_job?(site)
+      next if ["RefreshLinksWorker", "CreateLinksWorker"].include?(site.read_with) && LinkQueue.new(domain: site.domain).any?
+      { klass: site.read_with, arguments: {domain: site.domain} }
+    end.compact
   end
 
   def should_add_job?(site)
-    $mutext.synchronize { site.should_read? && !any_read_jobs_in_flight_with_domain?(site.domain) }
+    site.should_read? && !any_read_jobs_in_flight_with_domain?(site.domain)
   end
 
   def any_read_jobs_in_flight_with_domain?(domain)
-    $mutex.synchronize {
-      WORKERS.detect do |class_name|
-        klass = Object.const_get class_name
-        klass.jobs_in_flight_with_domain(domain).any?
-      end
-    }
+    WORKERS.detect do |class_name|
+      klass = Object.const_get class_name
+      klass.jobs_in_flight_with_domain(domain).any?
+    end
   end
 end
