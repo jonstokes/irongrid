22839a378354c7a62b1c6db0ba005bc7483335d8
Jon Stokes <jon@paperhatpress.com>
2014-04-07 17:24:39 -0500

    Fix to previous

--- app/services/site_stats_service.rb
+++ app/services/site_stats_service.rb
@@ -2,7 +2,7 @@ class SiteStatsService < CoreService
   SLEEP_INTERVAL = Rails.env.test? ? 1 : 86400
 
   def jobs
-    return unless should_add_jobs?
+    return [] unless should_add_jobs?
     Site.active.map do |site|
       { klass: "SiteStatsWorker", arguments: {domain: site.domain} }
     end.compact
