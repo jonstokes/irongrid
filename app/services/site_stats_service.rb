008ee267a317c15aba3b58e952f2ae7d9f467e80
Jon Stokes <jon@paperhatpress.com>
2014-04-07 14:33:58 -0500

    Only run SiteStatsWorker once per day

--- app/services/site_stats_service.rb
+++ app/services/site_stats_service.rb
@@ -1,4 +1,6 @@
 class SiteStatsService < CoreService
+  SLEEP_INTERVAL = Rails.env.test? ? 1 : 86400
+
   def jobs
     Site.active.map do |site|
       { klass: "SiteStatsWorker", arguments: {domain: site.domain} }
