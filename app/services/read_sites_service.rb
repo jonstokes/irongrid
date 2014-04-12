91a0c6e4cfccb3b80434f63b7b73003c01f0a786
Jon Stokes <jon@paperhatpress.com>
2014-04-12 13:17:04 -0500

    ReadLinksService: for now, make sure it empties out the link queue before trying to refresh again

--- app/services/read_sites_service.rb
+++ app/services/read_sites_service.rb
@@ -8,13 +8,6 @@ class ReadSitesService < CoreService
   end
 
   def should_add_job?(site)
-    site.should_read? && !any_read_jobs_in_flight_with_domain?(site.domain)
-  end
-
-  def any_read_jobs_in_flight_with_domain?(domain)
-    %w(CreateLinksWorker ScrapePagesWorker).detect do |class_name|
-      klass = Object.const_get class_name
-      klass.jobs_in_flight_with_domain(domain).any?
-    end
+    site.should_read? && LinkQueue.new(domain: site.domain).empty?
   end
 end
--- spec/services/read_sites_service_spec.rb
+++ spec/services/read_sites_service_spec.rb
@@ -17,19 +17,29 @@ describe ReadSitesService do
       @lq.clear
     end
 
-    it "should generate a RefreshLinksWorker for a site if it should be read" do
+    it "should generate a RefreshLinksWorker for a site if it should be read and its LinkQueue is empty" do
       @site.update(read_at: 10.days.ago)
       @service.start
       @service.stop
       expect(RefreshLinksWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
     end
 
-    it "should not generate a RefreshLinksWorker for a site if it should not be read" do
+    it "should not generate a RefreshLinksWorker for a site if it should not be read and its LinkQueue is empty" do
       @site.update(read_at: Time.now)
       @service.start
       @service.stop
       expect(RefreshLinksWorker.jobs_in_flight_with_domain(@site.domain)).to be_empty
     end
+
+    it "should not generate a RefreshLinksWorker for a site if it should be read and its LinkQueue is not empty" do
+      @site.update(read_at: Time.now)
+      @lq = LinkQueue.new(domain: @site.domain)
+      @lq.push "http://#{@site.domain}/1"
+      @service.start
+      @service.stop
+      expect(RefreshLinksWorker.jobs_in_flight_with_domain(@site.domain)).to be_empty
+    end
+
   end
 
   describe "CreateLinksWorker sites" do
@@ -46,14 +56,6 @@ describe ReadSitesService do
       @service.start
       @service.stop
       expect(CreateLinksWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
-    end
-
-    it "should not generate a CreateLinksWorker for a site if that site is already being read" do
-      @site.update(read_at: 10.days.ago)
-      CreateLinksWorker.perform_async(domain: @site.domain)
-      @service.start
-      @service.stop
-      expect(CreateLinksWorker.jobs_in_flight_with_domain(@site.domain).count).to eq(1)
     end
   end
 
