e05576555936e22c88aa32ac77463278ec6daac2
Jon Stokes <jon@paperhatpress.com>
2014-04-17 16:40:19 -0500

    DRY up ImageQueue with inheritance

--- app/models/image_queue.rb
+++ app/models/image_queue.rb
@@ -1,8 +1,6 @@
 class ImageQueue < LinkMessageQueue
   include IrongridRedisPool
 
-  attr_reader :domain, :set_name
-
   def initialize(opts)
     raise "Domain required!" unless @domain = opts[:domain]
     @set_name = "#imageset--#{domain}"
@@ -27,63 +25,20 @@ class ImageQueue < LinkMessageQueue
     end
   end
 
-  def rem(keys)
-    return 0 unless keys
-    return 0 if keys.is_a?(String) && keys.blank?
-
-    if keys.is_a?(Array)
-      return 0 if keys.empty?
-      keys = keys.uniq.select { |key| !key.empty? }
-    else
-      keys = [keys]
-    end
-
-    with_redis { |conn| conn.srem(set_name, keys) }
-    keys.count
-  end
-
   def clear
     with_redis do |conn|
       conn.del(set_name)
     end
   end
 
-  def empty?
-    with_redis do |conn|
-      return true unless conn.exists(set_name)
-      conn.scard(set_name) == 0
-    end
-  end
-
-  def members
-    with_redis do |conn|
-      conn.smembers(set_name)
-    end
-  end
-
-  def any?
-    !empty?
-  end
-
-  def has_key?(key)
-    with_redis do |conn|
-      return false unless key.present? && conn.exists(set_name)
-      return true if conn.sismember(set_name, key)
-    end
-  end
-
-  def size
-    with_redis do |conn|
-      return 0 unless conn.exists(set_name)
-      conn.scard(set_name)
-    end
-  end
-
   alias add push
-  alias length size
-  alias count size
 
   private
+
+  def remove_keys_from_redis(keys)
+    with_redis { |conn| conn.srem(set_name, keys) }
+    keys.count
+  end
 
   def is_valid_url?(key)
     !!URI.parse(key).host rescue false
