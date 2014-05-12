28a081612b97752ef09a573f29c72a08b8640ba4
Jon Stokes <jon@paperhatpress.com>
2014-05-12 12:29:32 -0500

    Some fixes to prev. Specs still green

--- app/interactors/extract_metadata_from_source_attributes.rb
+++ app/interactors/extract_metadata_from_source_attributes.rb
@@ -27,36 +27,46 @@ class ExtractMetadataFromSourceAttributes
     normalized_content = ProductDetails::Caliber.analyze(context[field_name].scrubbed)
     results = ProductDetails::Caliber.parse(normalized_content)
     context[field_name].normalized = results[:text]
-    context[:caliber] ||= ElasticSearchObject.new("caliber")
-    context[:caliber].raw = results[:keywords].first
-    context[:caliber].classification_type = "metadata"
-    context[:caliber_category] ||= ElasticSearchObject.new("caliber_category")
-    context[:caliber_category].raw = results[:category]
-    context[:caliber_category].classification_type = "metadata"
+    context[:caliber] = ElasticSearchObject.new(
+      "caliber",
+      raw: results[:keywords].first,
+      classification_type: "metadata"
+    )
+    context[:caliber_category] = ElasticSearchObject.new(
+      "caliber_category",
+      raw: results[:category],
+      classification_type: "metadata"
+    )
   end
 
   def extract_manufacturer(field_name)
     normalized_content = ProductDetails::Manufacturer.analyze(context[field_name].normalized || context[field_name].scrubbed)
     results = ProductDetails::Manufacturer.parse(normalized_content)
     context[field_name].normalized = results[:text]
-    context[:manufacturer] ||= ElasticSearchObject.new("manufacturer")
-    context[:manufacturer].raw = results[:keywords].first
-    context[:manufacturer].classification_type = "metadata"
+    context[:manufacturer] = ElasticSearchObject.new(
+      "manufacturer",
+      raw: results[:keywords].first,
+      classification_type: "metadata"
+    )
   end
 
   def extract_grains(field_name)
     results = ProductDetails::Grains.parse(context[field_name].scrubbed)
     context[field_name].normalized = results[:text]
-    context[:grains] ||= ElasticSearchObject.new("grains")
-    context[:grains].raw = results[:keywords].first
-    context[:grains].classification_type = "metadata"
+    context[:grains] = ElasticSearchObject.new(
+      "grains",
+      raw: results[:keywords].first,
+      classification_type: "metadata"
+    )
   end
 
   def extract_number_of_rounds(field_name)
     results = ProductDetails::Rounds.parse(context[field_name].normalized)
     context[field_name].normalized = results[:text]
-    context[:number_of_rounds] ||= ElasticSearchObject.new("number_of_rounds")
-    context[:number_of_rounds].raw = results[:keywords].first
-    context[:number_of_rounds].classification_type = "metadata"
+    context[:number_of_rounds] = ElasticSearchObject.new(
+      "number_of_rounds",
+      raw: results[:keywords].first,
+      classification_type: "metadata"
+    )
   end
 end
