f342b67fcba1c67059fe8b23e00dc2b8302c7cbd
Jon Stokes <jon@paperhatpress.com>
2014-05-12 12:10:05 -0500

    Extract metadata from attrs: first few specs work, but this isn't right

--- app/interactors/extract_metadata_from_source_attributes.rb
+++ app/interactors/extract_metadata_from_source_attributes.rb
@@ -4,9 +4,10 @@ class ExtractMetadataFromSourceAttributes
   def perform
     attributes_to_be_extracted.each do |attr|
       next if context[attr.to_sym].try(:raw)
-      next if send("extract_#{attr}", context[:title])
-      next unless keywords.raw
-      send("extract_#{attr}", context[:keywords])
+      send("extract_#{attr}", :title)
+      next if context[attr.to_sym].try(:raw) || !keywords.raw
+      puts "Extracting #{attr} from keywords"
+      send("extract_#{attr}", :keywords)
     end
     context[:title].normalized = ProductDetails.renormalize_all(title.normalized)
   end
@@ -22,34 +23,40 @@ class ExtractMetadataFromSourceAttributes
     end
   end
 
-  def extract_caliber(field)
-    normalized_content = ProductDetails::Caliber.analyze(field.scrubbed)
+  def extract_caliber(field_name)
+    normalized_content = ProductDetails::Caliber.analyze(context[field_name].scrubbed)
     results = ProductDetails::Caliber.parse(normalized_content)
-    field.normalized = results[:text]
+    context[field_name].normalized = results[:text]
     context[:caliber] ||= ElasticSearchObject.new("caliber")
     context[:caliber].raw = results[:keywords].first
+    context[:caliber].classification_type = "metadata"
+    context[:caliber_category] ||= ElasticSearchObject.new("caliber_category")
     context[:caliber_category].raw = results[:category]
+    context[:caliber_category].classification_type = "metadata"
   end
 
-  def extract_manufacturer(field)
-    normalized_content = ProductDetails::Manufacturer.analyze(field.normalized || field.scrubbed)
+  def extract_manufacturer(field_name)
+    normalized_content = ProductDetails::Manufacturer.analyze(context[field_name].normalized || context[field_name].scrubbed)
     results = ProductDetails::Manufacturer.parse(normalized_content)
-    field.normalized = results[:text]
+    context[field_name].normalized = results[:text]
     context[:manufacturer] ||= ElasticSearchObject.new("manufacturer")
     context[:manufacturer].raw = results[:keywords].first
+    context[:manufacturer].classification_type = "metadata"
   end
 
-  def extract_grains(field)
-    results = ProductDetails::Grains.parse(field)
-    field.normalized = results[:text]
+  def extract_grains(field_name)
+    results = ProductDetails::Grains.parse(context[field_name].scrubbed)
+    context[field_name].normalized = results[:text]
     context[:grains] ||= ElasticSearchObject.new("grains")
     context[:grains].raw = results[:keywords].first
+    context[:grains].classification_type = "metadata"
   end
 
-  def extract_number_of_rounds(field)
-    results = ProductDetails::Rounds.parse(field.normalized)
-    field.normalized = results[:text]
+  def extract_number_of_rounds(field_name)
+    results = ProductDetails::Rounds.parse(context[field_name].normalized)
+    context[field_name].normalized = results[:text]
     context[:number_of_rounds] ||= ElasticSearchObject.new("number_of_rounds")
-    context[:number_of_rounds] = results[:keywords].first
+    context[:number_of_rounds].raw = results[:keywords].first
+    context[:number_of_rounds].classification_type = "metadata"
   end
 end
--- spec/interactors/extract_metadata_from_source_attributes_spec.rb
+++ spec/interactors/extract_metadata_from_source_attributes_spec.rb
@@ -9,25 +9,40 @@ describe ExtractMetadataFromSourceAttributes do
         raw: "Ammunition",
         classificatio_type: "hard"
       )
-      @raw_listing = {
-        "title" => "Federal XM855 5.56 Ammo 62 Grain FMJ, 420 Rounds, Stripper Clips in Ammo Can",
-        "url" => @url,
-      }
+
+      @title_string = "Federal XM855 5.56 Ammo 62 Grain FMJ, 420 Rounds, Stripper Clips in Ammo Can"
+      @title = ElasticSearchObject.new(
+        "title",
+        raw: @title_string,
+        scrubbed: ProductDetails::Scrubber.scrub_all(@title_string)
+      )
+
+      @keywords_string = "Federal"
+      @keywords = ElasticSearchObject.new(
+        "keywords",
+        raw: @keywords_string,
+        scrubbed: ProductDetails::Scrubber.scrub_all(@keywords_string)
+      )
     end
 
     describe "manufacturer" do
       it "correctly extracts the manufacturer from the title" do
-        clean_listing = RetailListingCleaner.new(raw_listing: @raw_listing, url: @url, site: @site)
-        clean_listing.to_h['item_data']['manufacturer'].should == [{"manufacturer"=>"Federal"}, {"classification_type"=>"metadata"}]
+        result = ExtractMetadataFromSourceAttributes.perform(
+          category1: @category1,
+          title: @title
+        )
+        expect(result.manufacturer.raw).to eq("Federal")
+        expect(result.manufacturer.classification_type).to eq("metadata")
       end
 
       it "correctly extracts the manufacturer from the keywords" do
-        @raw_listing.merge!(
-          "title" => "Ammo, 420 Rounds",
-          "keywords" => "Federal"
+        result = ExtractMetadataFromSourceAttributes.perform(
+          category1: @category1,
+          keywords: @keywords,
+          title: ElasticSearchObject.new("title", raw: "Foobar", scrubbed: "Foobar")
         )
-        clean_listing = RetailListingCleaner.new(raw_listing: @raw_listing, url: @url, site: @site)
-        clean_listing.to_h['item_data']['manufacturer'].should == [{"manufacturer"=>"Federal"}, {"classification_type"=>"metadata"}]
+        expect(result.manufacturer.raw).to eq("Federal")
+        expect(result.manufacturer.classification_type).to eq("metadata")
       end
     end
 
