7ff100673fe08d753c2023a30b2ac7043f43d424
Jon Stokes <jon@paperhatpress.com>
2014-05-13 10:11:42 -0500

    Squash with prev

---
+++ spec/interactors/set_digest_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe SetDigest do
+  it "should correctly digest a standard, in-stock retail listing from Hyatt Gun Store" do
+    pending "Example"
+  end
+
+  it "should add the URL to the digest on a site where that's required" do
+    pending "Example"
+  end
+
+end
