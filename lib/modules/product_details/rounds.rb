c556b103bca46d3016237696d90952b702c5fa76
Jon Stokes <jon@paperhatpress.com>
2014-06-06 15:45:37 -0500

    Fixes for rounds parser

--- lib/modules/product_details/rounds.rb
+++ lib/modules/product_details/rounds.rb
@@ -3,7 +3,7 @@ module ProductDetails
     def self.parse(text)
       return unless text && !text.strip.blank?
       str = text.dup
-      rounds = str[/\d{2,5} round/i] ? str[/\d{2,5} round/i].delete(" rounds").delete(" round").to_i : nil
+      rounds = str[/\d{1,5} round/i] ? str[/\d{1,5} round/i].delete(" rounds").delete(" round").to_i : nil
       { text: str, keywords: [rounds].compact }
     end
   end
