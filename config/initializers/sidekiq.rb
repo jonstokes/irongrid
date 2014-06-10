b66d3efc50568d44cfe63671aee38c741301427f
Jon Stokes <jon@paperhatpress.com>
2014-06-10 10:35:28 -0500

    Manually patched in Airbrake support per http://goo.gl/TOfwqE

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 Sidekiq.configure_server do |config|
+  config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
   config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => ENV['RAILS_ENV'] }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
@@ -10,6 +11,7 @@ end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
+  config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
   config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => ENV['RAILS_ENV'] }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
