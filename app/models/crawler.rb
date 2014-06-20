93ef7e0aed2e9eb745952337c3257c567079404f
Jon Stokes <jon@paperhatpress.com>
2014-06-20 17:42:32 -0500

    Turned crawler into model

---
+++ app/models/crawler.rb
@@ -0,0 +1,64 @@
+class Crawler
+  include PageUtils
+
+  %w(product_link_xpath follow_link_prefix follow_link_xpaths urls).each do |key|
+    define_method key do
+      @sources[key]
+    end
+  end
+
+  def initialize(filename)
+    @sources = YAML.load_file(filename)
+    @visited = Set.new
+    @final_url_set = Set.new
+    @url_q = @sources["urls"].uniq
+  end
+
+  def crawl_links
+    while url = @url_q.pop do
+      puts "Crawling #{url}"
+      next unless page = visit(url)
+      @final_url_set << url if is_product_page?(page)
+      extract_links(page)
+    end
+  end
+
+  def write_yaml
+    feed_hash = { "feeds" => feeds }
+    File.open("tmp/link_sources.yml", "w") do |f|
+      f.puts feed_hash.to_yaml
+    end
+  end
+
+  def extract_links(page)
+    follow_link_xpaths.each do |xpath|
+      page.doc.xpath(xpath).each do |node|
+        url = "#{follow_link_prefix}#{node.text.strip}"
+        @url_q << url unless visited?(url)
+      end
+    end
+  end
+
+  def visited?(url)
+    @visited.include?(url)
+  end
+
+  def visit(url)
+    @visited << url
+    get_page(url)
+  end
+
+  def is_product_page?(page)
+    page.doc.xpath(product_link_xpath).any?
+  end
+
+
+  def feeds
+    @final_url_set.map do |url|
+      {
+        "url" => url,
+        "product_link_xpath" => product_link_xpath
+      }
+    end
+  end
+end
--- lib/tasks/crawl.rake
+++ lib/tasks/crawl.rake
@@ -1,63 +1,6 @@
-def sources
-  @sources
-end
-
-def visited?(url)
-  @visited.include?(url)
-end
-
-def visit(url)
-  @visited << url
-  get_page(url)
-end
-
-def initialize_sets
-  @sources = YAML.load_file("tmp/link_input.yml")
-  @visited = Set.new
-  @final_url_set = Set.new
-  @url_q = sources["urls"].uniq
-end
-
-def is_product_page?(page)
-  page.doc.xpath(sources["product_link_xpath"]).any?
-end
-
-def extract_links(page)
-  page.doc.xpath(sources["follow_link_xpath"]).each do |node|
-    url = "#{sources['follow_link_prefix']}#{node.text}"
-    @url_q << url unless visited?(url)
-  end
-end
-
-def crawl_links
-  while url = @url_q.pop do
-    puts "Crawling #{url}"
-    next unless page = visit(url)
-    @final_url_set << url if is_product_page?(page)
-    extract_links(page)
-  end
-end
-
-def feeds
-  @final_url_set.map do |url|
-    {
-      "url" => url,
-      "product_link_xpath" => sources["product_link_xpath"]
-    }
-  end
-end
-
-def write_yaml
-  feed_hash = { "feeds" => feeds }
-  File.open("tmp/link_sources.yml", "w") do |f|
-    f.puts feed_hash.to_yaml
-  end
-end
-
 task :crawl => :environment do
-  include PageUtils
-  initialize_sets
-  crawl_links
-  write_yaml
+  crawler = Crawler.new("tmp/link_input.yml")
+  crawler.crawl_links
+  crawler.write_yaml
 end
 
