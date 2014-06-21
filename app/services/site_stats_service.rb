211fb9c947a08293525262b82665cc277233f2f0
Jon Stokes <jon@paperhatpress.com>
2014-06-21 08:30:04 -0500

    SiteStatsService: this shouldn't spam the queue after every reboot

--- app/services/site_stats_service.rb
+++ app/services/site_stats_service.rb
@@ -2,13 +2,13 @@ class SiteStatsService < CoreService
   SLEEP_INTERVAL = Rails.env.test? ? 1 : 86400
 
   def jobs
-    return [] unless should_add_jobs?
     Site.all.map do |site|
+      next unless should_add_job?(site)
       { klass: "SiteStatsWorker", arguments: {domain: site.domain} }
     end.compact
   end
 
-  def should_add_jobs?
-    SiteStatsWorker.active_workers.empty? && SiteStatsWorker.queued_jobs.empty?
+  def should_add_job?
+    Time.now - site.stats.try(:[],:updated_at) > 86400
   end
 end
