449da4e75f10957a53f0f2da87f65b12f79f05c3
Jon Stokes <jon@paperhatpress.com>
2014-06-23 17:29:59 -0500

    This is a candidate for testing

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -5,29 +5,21 @@ require "capybara/poltergeist/utility"
 module Capybara::Poltergeist
   Client.class_eval do
     def start
-      puts "Entering popen..."
-      IO.popen(command.map(&:to_s)) do |pipe|
-        @read_io = pipe
-        puts "  Spawning out_thread..."
-        @out_thread = Thread.new {
-          while !@read_io.closed? && !@read_io.eof? && data = @read_io.readpartial(1024)
-            @phantomjs_logger.write(data)
-          end
-        }
-        puts "  Out_thread spawned"
-        @pid = pipe.pid
-      end
-      puts "Exited popen"
+      @read_io = IO.popen(command.map(&:to_s))
+      @pid = @read_io.pid
+      @out_thread = Thread.new {
+        while !@read_io.closed? && !@read_io.eof? && data = @read_io.readpartial(1024)
+          @phantomjs_logger.write(data)
+        end
+      }
     end
 
     def stop
-      puts "Stopping..."
       if pid
         kill_phantomjs
         @out_thread.kill if @out_thread
         @read_io.close if @read_io && !@read_io.closed?
       end
-      puts "Stopped!"
     end
   end
 end
