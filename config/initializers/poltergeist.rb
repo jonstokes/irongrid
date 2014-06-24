40766b17a274fd3680fa145378e723e8fef3cb12
Jon Stokes <jon@paperhatpress.com>
2014-06-24 08:26:50 -0500

    A fix, but needs testing

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -5,12 +5,21 @@ require "capybara/poltergeist/utility"
 module Capybara::Poltergeist
   Client.class_eval do
     def start
-      @pid = IO.popen(command.map(&:to_s)).pid
+      @io = IO.popen(command.map(&:to_s))
+      close_io
+      @pid = @io.pid
     end
 
     def stop
       if pid
         kill_phantomjs
+      end
+    end
+
+    def close_io
+      begin
+        @io.close unless @io.closed?
+      rescue IOError
       end
     end
   end
