db23d9453a837cb105d23434b551b99108fbb8cf
Jon Stokes <jon@paperhatpress.com>
2014-06-23 20:30:30 -0500

    Fix t previous

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -5,7 +5,7 @@ require "capybara/poltergeist/utility"
 module Capybara::Poltergeist
   Client.class_eval do
     def start
-      @pid = Process.spawn(*command.map(&:to_s), process_options)
+      @pid = Process.spawn(*command.map(&:to_s), pgroup: true)
       ObjectSpace.define_finalizer(self, self.class.process_killer(@pid))
     end
 
