fbbaa7875dcbafcf39b7b8bc546967c0d1507d31
Jon Stokes <jon@paperhatpress.com>
2014-06-23 19:29:03 -0500

    Another stab at a IO.popen implementation

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -5,20 +5,12 @@ require "capybara/poltergeist/utility"
 module Capybara::Poltergeist
   Client.class_eval do
     def start
-      @read_io = IO.popen(command.map(&:to_s))
-      @pid = @read_io.pid
-      @out_thread = Thread.new {
-        while !@read_io.closed? && !@read_io.eof? && data = @read_io.readpartial(1024)
-          @phantomjs_logger.write(data)
-        end
-      }
+      @pid = IO.popen(command.map(&:to_s)).pid
     end
 
     def stop
       if pid
         kill_phantomjs
-        @out_thread.kill if @out_thread
-        @read_io.close if @read_io && !@read_io.closed?
       end
     end
   end
