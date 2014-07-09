31eb5bf3326e98194f013c8df565cb0b3796e625
Jon Stokes <jon@paperhatpress.com>
2014-07-09 09:52:11 -0500

    Try to catch DHTTP and poltergeist errors

--- lib/modules/page_utils/dynamic_http.rb
+++ lib/modules/page_utils/dynamic_http.rb
@@ -3,12 +3,26 @@ require 'capybara/poltergeist'
 
 module PageUtils
   class DynamicHTTP
+    include Notifier
+
     attr_reader :session
 
     def initialize(opts = {})
       @opts = opts
+      new_session
+    end
+
+    def quit!
+      @session.driver.quit
+    end
+
+    def new_session
       Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(app, js_errors: false, phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'])
+        Capybara::Poltergeist::Driver.new(
+          app,
+          js_errors: false,
+          phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']
+        )
       end
       Capybara.default_driver = :poltergeist
       Capybara.javascript_driver = :poltergeist
@@ -18,10 +32,6 @@ module PageUtils
       @session
     end
 
-    def quit!
-      @session.driver.quit
-    end
-
     #
     # Create new Pages from the response of an HTTP request to *url*,
     # including redirects
@@ -29,6 +39,7 @@ module PageUtils
     def fetch_page(url, opts={})
       force_format = opts[:force_format]
       begin
+        tries ||= 5
         session.visit(url.to_s)
         page = PageUtils::Page.new(
           session.current_url,
@@ -37,12 +48,25 @@ module PageUtils
           :headers => session.response_headers,
           :force_format => force_format
         )
+        session.reset!
         return page
-      rescue Exception => e
-        return Page.new(url, :error => e)
+      rescue Capybara::Poltergeist::TimeoutError, Capybara::Poltergeist::DeadClient, Errno::EPIPE => e
+        quit!
+        new_session
+        notify "### Error: #{clean_error(e)}. Retrying with #{phantomjs_count} phantomjs instances running."
+        retry unless (tries -= 1).zero?
+        quit!
+        raise e
       end
-    ensure
-      session.reset!
+    end
+
+    def clean_error(e)
+      backtrace = e.backtrace.select { |line| !!line["home/bitnami/irongrid"] }
+      "#{e} #{backtrace}"
+    end
+
+    def phantomjs_count
+      `ps aux | grep phantomjs | wc -l`.strip
     end
 
     #
