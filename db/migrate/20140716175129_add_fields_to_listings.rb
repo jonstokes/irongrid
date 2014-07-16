83564d33c4b438ff84e952e61d1e1f4195a8c9c9
Jon Stokes <jon@paperhatpress.com>
2014-07-16 13:42:40 -0500

    Created migration to add fields and indexes to speed up common queries

---
+++ db/migrate/20140716175129_add_fields_to_listings.rb
@@ -0,0 +1,19 @@
+class AddFieldsToListings < ActiveRecord::Migration
+  def change
+    add_column :listings, :domain,                   :string
+    add_column :listings, :auction_ends,             :datetime
+    add_column :listings, :image,                    :string
+    add_column :listings, :image_download_attempted, :boolean, default: false
+
+    add_index "listings", ["domain"],       name: "index_listings_on_domain",       unique: false, using: :btree
+    add_index "listings", ["image"],        name: "index_listings_on_image",        unique: false, using: :btree
+    add_index "listings", ["auction_ends"], name: "index_listings_on_auction_ends", unique: false, using: :btree
+    add_index "listings", ["updated_at"],   name: "index_listings_on_updated_at",   unique: false, using: :btree
+    add_index "listings", ["inactive"],     name: "index_listings_on_inactive",     unique: false, using: :btree
+
+    add_column :log_records, :domain, :string
+
+    add_index "log_records", ["domain"],   name: "index_log_records_on_domain",   unique: false, using: :btree
+    add_index "log_records", ["archived"], name: "index_log_records_on_archived", unique: false, using: :btree
+  end
+end
