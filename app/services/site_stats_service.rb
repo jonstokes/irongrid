8a832c0fb511fcf7c9b48b5fd4d0b76fbd92441e
Jon Stokes <jon@paperhatpress.com>
2014-07-21 12:08:26 -0500

    Site stats service should run stats every 4 hours

--- app/services/site_stats_service.rb
+++ app/services/site_stats_service.rb
@@ -1,5 +1,5 @@
 class SiteStatsService < CoreService
-  SLEEP_INTERVAL = Rails.env.test? ? 1 : 86400
+  SLEEP_INTERVAL = Rails.env.test? ? 1 : 4.hours.to_i
 
   def jobs
     Site.all.map do |site|
@@ -10,6 +10,6 @@ class SiteStatsService < CoreService
 
   def should_add_job?(site)
     return true unless time = site.stats.try(:[],:updated_at)
-    (Time.now - time) > 86400
+    (Time.now - time) > SLEEP_INTERVAL
   end
 end
