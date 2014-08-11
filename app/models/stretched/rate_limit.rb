e28905b396674b3448474b63ee3c5674cdb6fee3
Jon Stokes <jon@paperhatpress.com>
2014-08-11 11:43:22 -0500

    Add getters to ratelimit

--- app/models/stretched/rate_limit.rb
+++ app/models/stretched/rate_limit.rb
@@ -1,5 +1,12 @@
 module Stretched
   class RateLimit < Registration
+
+    %w(peak off_peak).each do |key|
+      define_method key do
+        @data[key] if @data
+      end
+    end
+
     def initialize(opts)
       @type, @key, @data = "RateLimit", opts[:key], opts[:data]
     end
