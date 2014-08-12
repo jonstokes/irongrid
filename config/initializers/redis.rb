af74cb20ce506a5e8ee6a7edc34fc9813dcd7a30
Jon Stokes <jon@paperhatpress.com>
2014-08-12 14:23:41 -0500

    INitialize stretched redis pool

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,5 @@ pool_size = Figaro.env.redis_pool.to_i rescue 10
 IRONGRID_REDIS_POOL = ConnectionPool.new(timeout: 5, size: pool_size) { Redis.new(url: Figaro.env.irongrid_redis_url) }
 IRONSIGHTS_REDIS_POOL = ConnectionPool.new(timeout: 5, size: pool_size) { Redis.new(url: Figaro.env.ironsights_redis_url) }
 VALIDATOR_REDIS_POOL = ConnectionPool.new(timeout: 5, size: pool_size) { Redis.new(url: Figaro.env.validator_redis_url) }
+STRETCHED_REDIS_POOL = ConnectionPool.new(timeout: 5, size: pool_size) { Redis.new(url: Figaro.env.stretched_redis_url) }
 
