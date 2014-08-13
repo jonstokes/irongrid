60c11ea1dec5d271e5fe86bc74ffa8e0e577d579
Jon Stokes <jon@paperhatpress.com>
2014-08-13 14:24:23 -0500

    Fixes for Pageutils

--- spec/interactors/stretched/extract_json_from_page_spec.rb
+++ spec/interactors/stretched/extract_json_from_page_spec.rb
@@ -5,10 +5,12 @@ describe Stretched::ExtractJsonFromPage do
 
   before :each do
     @domain = "www.budsgunshop.com"
+    @product_url = "http://#{@domain}/products/1"
   end
 
   describe "#perform" do
     it "should translate a page into JSON using a schema" do
+
       Mocktra(@domain) do
         get '/products/1' do
           File.open("#{Rails.root}/spec/fixtures/web_pages/www--budsgunshop--com/product1.html") do |file|
@@ -17,7 +19,10 @@ describe Stretched::ExtractJsonFromPage do
         end
       end
 
-      #stuff
+      page = Stretched::PageUtils::Test.get_page(@product_url)
+
+      expect(page).not_to be_nil
+      expect(page.doc).not_to be_nil
 
     end
   end
