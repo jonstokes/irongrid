7b8238bfe39caa28b34d27628b4c178183fc5555
Jon Stokes <jon@paperhatpress.com>
2014-08-14 13:56:09 -0500

    JSON specs are green

--- app/models/stretched/instance_hash.rb
+++
@@ -1,6 +0,0 @@
-module Stretched
-  class InstanceHash < Hash
-    include Hashie::Extensions::MethodAccess
-    include Hashie::Extensions::IndifferentAccess
-  end
-end
--- spec/interactors/stretched/extract_json_from_page_spec.rb
+++ spec/interactors/stretched/extract_json_from_page_spec.rb
@@ -33,10 +33,12 @@ describe Stretched::ExtractJsonFromPage do
       expect(result.json_objects).not_to be_empty
 
       listing = result.json_objects.first
-      puts "#{listing.to_yaml}"
       expect(listing['title']).to include("Ruger 3470 SR40 15+1 40S&")
       expect(listing['keywords']).to include("Ruger, 3470")
+      expect(listing['location']).to eq("1105 Industry Road Lexington, KY 40505")
       expect(listing['image']).to eq("http://www.budsgunshop.com/catalog/images/69980_1.jpg")
+      expect(listing['product_caliber']).to eq("Caliber   410/45 Long Colt")
+      expect(listing['sale_price_in_cents']).to be_nil
     end
 
     it "should translate a page into JSON using a script" do
@@ -58,11 +60,38 @@ describe Stretched::ExtractJsonFromPage do
       expect(result.json_objects).not_to be_empty
 
       listing = result.json_objects.first
-      puts "#{listing.to_yaml}"
+      expect(listing['title']).to include("Ruger 3470 SR40 15+1 40S&")
+      expect(listing['type']).to eq("RetailListing")
+      expect(listing['image']).to eq("http://www.budsgunshop.com/catalog/images/69980_1.jpg")
+      expect(listing['product_caliber']).to eq("410/45 Long Colt")
+      expect(listing['location']).to be_nil
+    end
+
+    it "should translate a page into JSON using JSON and scripts combined" do
+      Mocktra(@domain) do
+        get '/products/1' do
+          File.open("#{Rails.root}/spec/fixtures/web_pages/www--budsgunshop--com/product1.html") do |file|
+            file.read
+          end
+        end
+      end
+
+      page = Stretched::PageUtils::Test.get_page(@product_url)
+
+      result = Stretched::ExtractJsonFromPage.perform(
+        page: page,
+        adapter_name: "www.budsgunshop.com/product_page"
+      )
+
+      expect(result.json_objects).not_to be_empty
+
+      listing = result.json_objects.first
       expect(listing['title']).to include("Ruger 3470 SR40 15+1 40S&")
       expect(listing['type']).to eq("RetailListing")
       expect(listing['location']).to eq("1105 Industry Road Lexington, KY 40505")
       expect(listing['image']).to eq("http://www.budsgunshop.com/catalog/images/69980_1.jpg")
+      expect(listing['product_caliber']).to eq("410/45 Long Colt")
+      expect(listing['sale_price_in_cents']).to eq(41100)
     end
 
     it "errors if the JSON adapter has an invalid attribute that doesn't match the schema" do
