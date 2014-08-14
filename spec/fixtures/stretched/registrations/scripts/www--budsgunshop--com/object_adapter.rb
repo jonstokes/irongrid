202c2fc1314f3b25136e90da62d6d6c61d2bacba
Jon Stokes <jon@paperhatpress.com>
2014-08-13 19:22:28 -0500

    Interactor: basic scripting works, but still some kinks to iron out

--- spec/fixtures/stretched/registrations/scripts/www--budsgunshop--com/object_adapter.rb
+++ spec/fixtures/stretched/registrations/scripts/www--budsgunshop--com/object_adapter.rb
@@ -1,18 +1,23 @@
-attribute "listing_type" do
-  "RetailListing"
-end
-
-attribute "listing_location" do
-  "1105 Industry Road Lexington, KY 40505"
-end
-
-attribute "listing_title" do
-  meta_title ||
-    meta_og_title ||
-    find_by_xpath(xpath: ".//title")
-end
-
-attribute "listing_image" do
-  meta_og_image ||
-    filter(find_by_xpath(xpath: './/div[@style="padding:0px 8px 2px 8px;"]//img/@src', pattern: /images\/.*jpg/), [prefix: "http://www.budsgunshop.com/catalog/"]
+Stretched::Script.define do
+  script "www.budsgunshop.com/script" do
+    type "RetailListing"
+    title do
+      meta_title || meta_og_title || find_by_xpath(xpath: ".//title")
+    end
+    location "1105 Industry Road Lexington, KY 40505"
+    image { meta_og_image }
+    sale_price_in_cents do
+      find_by_xpath(
+        xpath: './/tr[@id="x2d"]/td[2]/div[1]/div[1]/div[2]/strong',
+        pattern: /^\$\d[\d\,\.]+/i
+      )
+    end
+    product_caliber do
+      result = find_by_xpath(
+            xpath: ".//div[@id='mainmain']//td[@class='main']/center/table//tr",
+            pattern: /caliber\W*.*$/i
+      )
+      filters(result, [ reject: /caliber/i])
+    end
+  end
 end
--- spec/interactors/stretched/extract_json_from_page_spec.rb
+++ spec/interactors/stretched/extract_json_from_page_spec.rb
@@ -10,10 +10,11 @@ describe Stretched::ExtractJsonFromPage do
     Stretched::Schema.create_from_file("spec/fixtures/stretched/registrations/schemas/listing.json")
     Stretched::Schema.create_from_file("spec/fixtures/stretched/registrations/schemas/product_link.json")
     Stretched::ObjectAdapter.create_from_file("spec/fixtures/stretched/registrations/object_adapters/www--budsgunshop--com.yml")
+    Stretched::Script.create_from_file("spec/fixtures/stretched/registrations/scripts/www--budsgunshop--com/object_adapter.rb")
   end
 
   describe "#perform" do
-    it "should translate a page into JSON using a schema" do
+    it "should translate a page into JSON using a JSON adapter" do
 
       Mocktra(@domain) do
         get '/products/1' do
@@ -39,6 +40,36 @@ describe Stretched::ExtractJsonFromPage do
       expect(listing['image']).to eq("http://www.budsgunshop.com/catalog/images/69980_1.jpg")
 
     end
+
+    it "should translate a page into JSON using a script" do
+
+      Mocktra(@domain) do
+        get '/products/1' do
+          File.open("#{Rails.root}/spec/fixtures/web_pages/www--budsgunshop--com/product1.html") do |file|
+            file.read
+          end
+        end
+      end
+
+      page = Stretched::PageUtils::Test.get_page(@product_url)
+
+      result = Stretched::ExtractJsonFromPage.perform(
+        page: page,
+        adapter_name: "www.budsgunshop.com/product_page_scripts_only"
+      )
+
+      expect(result.json_objects).not_to be_empty
+
+      listing = result.json_objects.first
+      puts "#{listing.to_yaml}"
+      expect(listing['title']).to include("Ruger 3470 SR40 15+1 40S&")
+      expect(listing['type']).to eq("RetailListing")
+      expect(listing['location']).to eq("1105 Industry Road Lexington, KY 40505")
+      expect(listing['image']).to eq("http://www.budsgunshop.com/catalog/images/69980_1.jpg")
+
+    end
+
+
   end
 
 end
