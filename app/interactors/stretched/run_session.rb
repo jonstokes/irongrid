5c8a14bdbb0a8215c14ab773d58ae094e0aadd2b
Jon Stokes <jon@paperhatpress.com>
2014-08-14 17:10:05 -0500

    More fixes

--- app/interactors/stretched/run_session.rb
+++ app/interactors/stretched/run_session.rb
@@ -1,16 +1,37 @@
 module Stretched
   class RunSession
     include Interactor
+    include Stretched::PageUtils
 
     def perform
       #context: stretched_session, browser_session
-      rate_limiter = Stretched::RateLimiter.new(stretched_session.rate_limit)
-
-      stretched_session.urls.each do |url|
-        page = fetch_page(url)
+      context[:json_objects] = stretched_session.urls.each do |url|
+        next unless page = scrape_page(url)
         stretched_session.object_adapters.each do |adapter|
-          objects = ExtractJsonFromPage(page: page, adapter: adapter)
+          object_q = ObjectQueue.find(adapter.queue_name)
+          object_q.add ExtractJsonFromPage.perform(
+            page: page,
+            adapter: adapter,
+            browser_session: browser_session
+          )
         end
+      end
+    ensure
+      close_http_connections
+    end
+
+    private
+
+    def browser_session
+      return unless @dhttp
+      @dhttp.session
+    end
+
+    def scrape_page(url)
+      if stretched_session.page_format == "dhtml"
+        stretched_session.with_limit { render_page(url) }
+      else
+        stretched_session.with_limit { get_page(url) }
       end
     end
 
