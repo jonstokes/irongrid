7842e1ae9bca4cfdaa81993c25483b6b812accf5
Jon Stokes <jon@paperhatpress.com>
2014-08-15 15:08:29 -0500

    RateLimit fixes

--- app/models/stretched/rate_limit.rb
+++ app/models/stretched/rate_limit.rb
@@ -7,9 +7,24 @@ module Stretched
       end
     end
 
+    delegate :with_limit, to: :limiter
+
     def initialize(opts)
       super(opts.merge(type: "RateLimit"))
+      @limiter = Stretched::RateLimiter.new(seconds_per_action)
     end
+
+    def seconds_per_action
+      return 5 unless @data
+      @data.each do |time_slot, attr|
+        start_time = ActiveSupport::TimeZone[timezone].parse(attr["start"])
+        duration = attr["duration"].to_i.hours
+        end_time = (start_time + duration).in_time_zone(myzone)
+        return attr["rate"] if (start_time..end_time).cover?(Time.zone.now)
+      end
+      return peak["rate"]
+    end
+
 
     def self.find(key)
       super(type: "RateLimit", key: key)
