a0acccbb34ddc5e7fab665a63e1a05a3539fc4e8
Jon Stokes <jon@paperhatpress.com>
2014-08-15 10:45:15 -0500

    Stubbed in session_definition

---
+++ app/models/stretched/session_queue.rb
@@ -0,0 +1,22 @@
+module Stretched
+  class SessionQueue < ObjectQueue
+
+    SESSION_PROPERTIES = %w(queue session_definition object_adapters urls)
+
+    private
+
+    def validate_session_format(objects)
+      objects.each do |object|
+        object.each do |key, value|
+          raise "Invalid session property #{key}" unless SESSION_PROPERTIES.include?(key)
+        end
+      end
+    end
+
+    def add_objects_to_redis(objects)
+      validate_session_format(objects)
+      super(objects)
+    end
+
+  end
+end
