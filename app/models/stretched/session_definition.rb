fdaf2a9759dc5c203f108958ac083bc28192204b
Jon Stokes <jon@paperhatpress.com>
2014-08-15 13:56:39 -0500

    Something is broken

--- app/models/stretched/session_definition.rb
+++ app/models/stretched/session_definition.rb
@@ -6,7 +6,7 @@ module Stretched
     delegate :with_limit, to: :rate_limits
 
     def initialize(opts)
-      super(opts.merge(type: SessionDefinition))
+      super(opts.merge(type: "SessionDefinition"))
       @page_format = opts[:page_format]
       @rate_limits = Stretched::Registration.find_or_create(opts[:rate_limits], RateLimit)
     end
