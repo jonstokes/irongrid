8b2b7d19a006d5d9836096fa989cca821fa5796e
Jon Stokes <jon@paperhatpress.com>
2014-08-18 15:31:46 -0500

    Added some basic global scripting and extensions

--- spec/fixtures/stretched/registrations/scripts/globals/conversions.rb
+++ spec/fixtures/stretched/registrations/scripts/globals/conversions.rb
@@ -1,11 +1,14 @@
 Stretched::Script.define do
   script "globals/conversions" do
+
     price_in_cents do |instance|
-      convert_dollars_to_cents(instance.price_in_cents) if instance.price_in_cents
+      convert_dollars_to_cents(instance.price_in_cents)
     end
+
     sale_price_in_cents do |instance|
-      convert_dollars_to_cents(instance.sale_price_in_cents) if instance.sale_price_in_cents
+      convert_dollars_to_cents(instance.sale_price_in_cents)
     end
+
   end
 end
 
