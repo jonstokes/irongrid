15c1027d617a88868f0f03412e7242c7a2c8287f
Jon Stokes <jon@paperhatpress.com>
2014-08-18 10:23:35 -0500

    DRY up extractor a little bit

--- app/models/stretched/definition_proxy.rb
+++ app/models/stretched/definition_proxy.rb
@@ -2,20 +2,18 @@ module Stretched
   class DefinitionProxy
     def script(script_name, &block)
       runner = ScriptRunner.new
-      #Extension.register_all
 
       # Define all extensions on the runner instance
       Extension.registry.each_pair do |extname, block|
         runner.instance_eval(&block)
       end
 
-      # Set up runner instance for later use
+      # Set up runner instance for use
       if block_given?
         runner.instance_eval(&block)
       end
 
-      # Register runner instance in global registry
-      Script.register(script_name, runner)
+      runner
     end
 
     def extension(script_name, &block)
