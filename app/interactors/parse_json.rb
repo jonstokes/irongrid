a9d67c78e5f6cd83b2044f37b0abf646092bfea6
Jon Stokes <jon@paperhatpress.com>
2014-08-19 16:37:04 -0500

    Stubbed in more specs

---
+++ app/interactors/parse_json.rb
@@ -0,0 +1,32 @@
+class ParseJson
+  include Interactor::Organizer
+
+  # Expect context to have site, adapter_type, page, url
+
+  organize [
+    ValidateListingPresence,
+    ValidateListing, # includes setting auction_ends
+    SetCommonAttributes,
+    ScrubMetadataSourceAttributes,
+    ExtractMetadataFromRawListing,
+    ExtractMetadataFromSourceAttributes,
+    SoftCategorize,
+    SetPricePerRound,
+    SetDigest,
+    GenerateListingHash
+  ]
+
+  def is_valid?
+    success?
+  end
+
+  def not_found?
+    !success? && status == :not_found
+  end
+
+  def classified_sold?
+    !success? && status == :classified_sold
+  end
+end
+
+
