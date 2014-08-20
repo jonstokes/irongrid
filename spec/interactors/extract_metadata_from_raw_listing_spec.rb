704983c231e906f6ad3d2f2c7bac19dbf83e195e
Jon Stokes <jon@paperhatpress.com>
2014-08-20 13:01:26 -0500

    Metadata from raw works

--- spec/interactors/extract_metadata_from_raw_listing_spec.rb
+++ spec/interactors/extract_metadata_from_raw_listing_spec.rb
@@ -9,19 +9,19 @@ describe ExtractMetadataFromRawListing do
         raw: "Ammunition",
         classification_type: "hard"
       )
-      @raw_listing = {
+      @listing_json = Hashie::Mash.new(
         "title" => "Federal XM855 5.56 Ammo 62 Grain FMJ, 420 Rounds, Stripper Clips in Ammo Can",
         "url" => @url,
-      }
+      )
     end
 
-    it "correctly extracts the manufacturer from the raw_listing if it's explicit there" do
-      @raw_listing.merge!(
+    it "correctly extracts the manufacturer from the listing_json if it's explicit there" do
+      @listing_json.merge!(
         "title" => "Ammo, 420 Rounds",
         "manufacturer" => "Mfgr: Federal"
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.manufacturer.raw).to eq("Federal")
@@ -29,51 +29,51 @@ describe ExtractMetadataFromRawListing do
     end
 
     it "correctly hard classifieds the caliber_category" do
-      @raw_listing.merge!(
+      @listing_json.merge!(
         "title" => "Federal Ammo, 420 Rounds",
         "caliber_category" => "rifle"
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.caliber_category.raw).to eq("rifle")
       expect(result.caliber_category.classification_type).to eq("hard")
     end
 
-    it "correctly extracts the caliber when it's explicitly present in raw_listing" do
-      @raw_listing.merge!(
+    it "correctly extracts the caliber when it's explicitly present in listing_json" do
+      @listing_json.merge!(
         "title" => "Federal Ammo, 420 Rounds",
         "caliber" => "Caliber: 20ga"
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.caliber.raw).to eq("20 gauge")
       expect(result.caliber.classification_type).to eq("hard")
     end
 
-    it "correctly extracts the number of rounds from the raw_listing when it's present there" do
-      @raw_listing.merge!(
+    it "correctly extracts the number of rounds from the listing_json when it's present there" do
+      @listing_json.merge!(
         "title" => "Federal XM855 .44 FMJ",
         "number_of_rounds" => "420",
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.number_of_rounds.raw).to eq(420)
       expect(result.number_of_rounds.classification_type).to eq("hard")
     end
 
-    it "correctly extracts the grains from the raw_listing if it's present there" do
-      @raw_listing.merge!(
+    it "correctly extracts the grains from the listing_json if it's present there" do
+      @listing_json.merge!(
         "title" => "Federal XM855 .44 FMJ",
         "grains" => "62",
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.grains.raw).to eq(62)
@@ -81,12 +81,12 @@ describe ExtractMetadataFromRawListing do
     end
 
     it "correctly soft classifies the caliber_category as rifle" do
-      @raw_listing.merge!(
+      @listing_json.merge!(
         "title" => "Federal Ammo, 420 Rounds",
         "caliber" => "Caliber: .223 Remington"
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.caliber_category.raw).to eq("rifle")
@@ -94,12 +94,12 @@ describe ExtractMetadataFromRawListing do
     end
 
     it "correctly soft classifies the caliber_category as shotgun" do
-      @raw_listing.merge!(
+      @listing_json.merge!(
         "title" => "Federal Ammo, 420 Rounds",
         "caliber" => "Caliber: 20ga"
       )
       result = ExtractMetadataFromRawListing.perform(
-        raw_listing: @raw_listing,
+        listing_json: @listing_json,
         category1: @category1
       )
       expect(result.caliber_category.raw).to eq("shotgun")
