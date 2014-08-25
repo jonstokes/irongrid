f2c5074ff0b866d2536c9d25fb53be7af92d3781
Jon Stokes <jon@paperhatpress.com>
2014-08-25 12:50:40 -0500

    SetPriceAttrs interactor specs are green

--- spec/interactors/set_price_attributes_spec.rb
+++ spec/interactors/set_price_attributes_spec.rb
@@ -7,28 +7,22 @@ describe SetPriceAttributes do
     end
 
     it "correctly sets prices for a retail listing" do
-      raw_listing = {
-        "validation" => {
-          "retail" => "(raw['price'] || raw['sale_price']) && raw['title'] && raw['image'] && raw['description']",
-        },
-        "seller_defaults"=> {
-          "condition"=>"new",
-          "listing_type"=>"retail",
-          "stock_status"=>"In Stock",
-          "item_location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705"
-        },
+      listing_json = Hashie::Mash.new(
+        "condition"=>"new",
+        "type"=>"retail",
+        "availability"=>"in_stock",
+        "location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705",
         "title" => "CITADEL 1911 COMPACT .45ACP 3 1/2\" HOGUE BLACK", 
         "keywords" => "CITADEL 1911 COMPACT .45ACP",
         "image" => "http://www.emf-company.com/store/pc/catalog/1911CITCSPHBat10MED.JPG",
-        "price" => "$650.00",
-        "sale_price" => "$600.00",
+        "price_in_cents" => 65000,
+        "sale_price_in_cents" => 60000,
         "description" => ".45ACP, 3 1/2\" BARREL, HOGUE BLACK GRIPS",
-        "category1" => "Guns"
-      }
+        "product_category1" => "Guns"
+      )
       result = SetPriceAttributes.perform(
         site: @site,
-        raw_listing: raw_listing,
-        adapter: @site.page_adapter,
+        listing_json: listing_json,
         type: "RetailListing"
       )
       expect(result.price_in_cents).to eq(65000)
@@ -37,57 +31,45 @@ describe SetPriceAttributes do
 
 
     it "correctly sets prices for a classified listing" do
-      raw_listing = {
-        "validation" => {
-          "classified" => "(raw['price'] || raw['sale_price']) && raw['title'] && raw['image'] && raw['description']",
-        },
-        "seller_defaults"=> {
-          "condition"=>"new",
-          "listing_type"=>"classified",
-          "stock_status"=>"In Stock",
-          "item_location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705"
-        },
-        "title" => "CITADEL 1911 COMPACT .45ACP 3 1/2\" HOGUE BLACK", 
+      listing_json = Hashie::Mash.new(
+        "condition"=>"new",
+        "type"=>"classified",
+        "availability"=>"in_stock",
+        "location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705",
+        "title" => "CITADEL 1911 COMPACT .45ACP 3 1/2\" HOGUE BLACK",
         "keywords" => "CITADEL 1911 COMPACT .45ACP",
         "image" => "http://www.emf-company.com/store/pc/catalog/1911CITCSPHBat10MED.JPG",
-        "price" => "$650.00",
+        "price_in_cents" => 65000,
         "description" => ".45ACP, 3 1/2\" BARREL, HOGUE BLACK GRIPS",
-        "category1" => "Guns"
-      }
+        "product_category1" => "Guns"
+      )
       result = SetPriceAttributes.perform(
         site: @site,
-        raw_listing: raw_listing,
-        adapter: @site.page_adapter,
+        listing_json: listing_json,
         type: "ClassifiedListing"
       )
       expect(result.price_in_cents).to eq(65000)
     end
 
     it "correctly sets prices for an auction listing" do
-      raw_listing = {
-        "validation" => {
-          "auction" => "(raw['price'] || raw['sale_price']) && raw['title'] && raw['image'] && raw['description']"
-        },
-        "seller_defaults"=> {
-          "condition"=>"new",
-          "listing_type"=>"auction",
-          "stock_status"=>"In Stock",
-          "item_location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705"
-        },
+      listing_json = Hashie::Mash.new(
+        "condition"=>"new",
+        "type"=>"auction",
+        "availability"=>"in_stock",
+        "location"=>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705",
         "title" => "CITADEL 1911 COMPACT .45ACP 3 1/2\" HOGUE BLACK", 
         "keywords" => "CITADEL 1911 COMPACT .45ACP",
         "image" => "http://www.emf-company.com/store/pc/catalog/1911CITCSPHBat10MED.JPG",
-        "current_bid" => "$1.00",
-        "buy_now_price" => "$2.00",
-        "minimum_bid" => "$3.00",
-        "reserve" => "$4.00",
+        "current_bid_in_cents" => 100,
+        "buy_now_price_in_cents" => 200,
+        "minimum_bid_in_cents" => 300,
+        "reserve_in_cents" => 400,
         "description" => ".45ACP, 3 1/2\" BARREL, HOGUE BLACK GRIPS",
-        "category1" => "Guns"
-      }
+        "product_category1" => "Guns"
+      )
       result = SetPriceAttributes.perform(
         site: @site,
-        raw_listing: raw_listing,
-        adapter: @site.page_adapter,
+        listing_json: listing_json,
         type: "ClassifiedListing"
       )
       expect(result.current_bid_in_cents).to eq(100)
