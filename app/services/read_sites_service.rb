1e9967e5d371a98b6b7c16f62853f20a2bf0717b
Jon Stokes <jon@paperhatpress.com>
2014-08-29 16:44:55 -0500

    ReadSitesService spec passes

--- app/services/read_sites_service.rb
+++ app/services/read_sites_service.rb
@@ -3,11 +3,11 @@ class ReadSitesService < CoreService
   def jobs
     Site.all.map do |site|
       next unless should_add_job?(site)
-      { klass: PopulateSessionQueue, arguments: {domain: site.domain} }
+      { klass: "PopulateSessionQueueWorker", arguments: {domain: site.domain} }
     end.compact
   end
 
   def should_add_job?(site)
-    site.should_read? && PopulateSessionQueue.jobs_in_flight_with_domain(site.domain).empty?
+    site.should_read? && PopulateSessionQueueWorker.jobs_in_flight_with_domain(site.domain).empty?
   end
 end
--- spec/services/read_sites_service_spec.rb
+++ spec/services/read_sites_service_spec.rb
@@ -16,66 +16,24 @@ describe ReadSitesService do
     pending "Example"
   end
 
-  describe "RefreshLinksWorker sites", no_es: true do
+  describe "PopulateSessionQueue sites", no_es: true do
     before :each do
       @site = create_site "www.retailer.com"
-      @site.update(read_with: "RefreshLinksWorker")
       @service = ReadSitesService.new
-      @lq = LinkMessageQueue.new(domain: @site.domain)
-      @lq.clear
     end
 
-    it "should generate a RefreshLinksWorker for a site if it should be read" do
+    it "should generate a PopulateSessionQueue for a site if it should be read" do
       @site.update(read_at: 10.days.ago)
       @service.start
       @service.stop
-      expect(RefreshLinksWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
+      expect(PopulateSessionQueueWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
     end
 
-    it "should not generate a RefreshLinksWorker for a site if it should not be read" do
+    it "should not generate a PopulateSessionQueue for a site if it should not be read" do
       @site.update(read_at: Time.now)
       @service.start
       @service.stop
-      expect(RefreshLinksWorker.jobs_in_flight_with_domain(@site.domain)).to be_empty
-    end
-  end
-
-  describe "CreateLinksWorker sites", no_es: true do
-    before :each do
-      @site = create_site "www.retailer.com"
-      @site.update(read_with: "CreateLinksWorker")
-      @service = ReadSitesService.new
-      @lq = LinkMessageQueue.new(domain: @site.domain)
-      @lq.clear
-    end
-
-    it "should read a CreateLinksWorker site if it should be read" do
-      @site.update(read_at: 10.days.ago)
-      @service.start
-      @service.stop
-      expect(CreateLinksWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
-    end
-
-    it "should not generate a CreateLinksWorker for a site if that site is already being read" do
-      @site.update(read_at: 10.days.ago)
-      CreateLinksWorker.perform_async(domain: @site.domain)
-      @service.start
-      @service.stop
-      expect(CreateLinksWorker.jobs_in_flight_with_domain(@site.domain).count).to eq(1)
-    end
-  end
-
-  describe "ProductFeedWorker sites", no_es: true do
-    it "should read an ProductFeedWorker site with the correct worker" do
-      @site = create_site "www.retailer.com"
-      @service = ReadSitesService.new
-      @lq = LinkMessageQueue.new(domain: @site.domain)
-      @lq.clear
-      @site.update(read_at: 10.days.ago)
-      @site.update(read_with: "ProductFeedWorker")
-      @service.start
-      @service.stop
-      expect(ProductFeedWorker.jobs_in_flight_with_domain(@site.domain).size).to eq(1)
+      expect(PopulateSessionQueueWorker.jobs_in_flight_with_domain(@site.domain)).to be_empty
     end
   end
 end
