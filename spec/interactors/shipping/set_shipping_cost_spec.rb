46edfdd7c1a8857c8d7fe2c67d7af5b0fd449753
Jon Stokes <jon@paperhatpress.com>
2014-09-12 17:04:06 -0500

    Shipping script specs green

--- spec/interactors/shipping/set_shipping_cost_spec.rb
+++ spec/interactors/shipping/set_shipping_cost_spec.rb
@@ -4,6 +4,7 @@ describe Shipping::SetShippingCost do
   describe "#perform", no_es: true do
     before :each do
       @site = create_site "www.budsgunshop.com"
+      Script.create_from_file("#{Rails.root}/spec/fixtures/scripts/www--budsgunshop--com.rb")
     end
 
     it "sets the shipping cost using the listing_json" do
@@ -14,5 +15,29 @@ describe Shipping::SetShippingCost do
       )
       expect(results.shipping_cost_in_cents).to eq(200)
     end
+
+    it "sets the shipping cost using a script" do
+      results = Shipping::SetShippingCost.perform(
+        product_category1: "Guns",
+        site: @site,
+        listing_json: Hashie::Mash.new
+      )
+      expect(results.shipping_cost_in_cents).to eq(0)
+
+      results = Shipping::SetShippingCost.perform(
+        product_category1: "Ammunition",
+        site: @site,
+        listing_json: Hashie::Mash.new
+      )
+      expect(results.shipping_cost_in_cents).to eq(995)
+
+      results = Shipping::SetShippingCost.perform(
+        product_category1: "Optics",
+        site: @site,
+        listing_json: Hashie::Mash.new
+      )
+      expect(results.shipping_cost_in_cents).to be_nil
+    end
+
   end
 end
