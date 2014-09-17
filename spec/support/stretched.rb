4721cbb09065dd89c3e5a217d06b8dcc6c3d6586
Jon Stokes <jon@paperhatpress.com>
2014-09-17 10:50:39 -0500

    Created new register_globals function for specs

--- spec/support/stretched.rb
+++ spec/support/stretched.rb
@@ -5,6 +5,18 @@ def register_stretched_globals
   Stretched::Registration.register_from_file("#{Rails.root}/spec/fixtures/stretched/registrations/globals.yml")
 end
 
+def register_globals
+  Dir["#{Rails.root}/spec/fixtures/stretched/registrations/globals/extensions/*.rb"].each do |filename|
+    Stretched::Extension.register_from_file(filename)
+  end
+
+  Dir["#{Rails.root}/spec/fixtures/stretched/registrations/globals/scripts/*.rb"].each do |filename|
+    Stretched::Extension.register_from_file(filename)
+  end
+
+  Stretched::Registration.register_from_file("#{Rails.root}/spec/fixtures/stretched/registrations/globals/registrations.yml")
+end
+
 def register_site(domain)
   source = YAML.load_file("#{Rails.root}/spec/fixtures/sites/#{domain.gsub(".","--")}.yml")['registrations']
   Stretched::Registration.register_from_source source
