174b6f09006a011f49cb2ba30e4cb2f388006436
Jon Stokes <jon@paperhatpress.com>
2014-09-17 16:59:21 -0500

    Stubbed in tokenizer code

--- app/models/stretched/mapping.rb
+++ app/models/stretched/mapping.rb
@@ -1,17 +1,42 @@
 module Stretched
   class Mapping < Registration
+    attr_reader :tokenizer
 
     def initialize(opts)
       super(opts.merge(type: "Mapping"))
+      @tokenizer = Tokenizer::Tokenizer.new
     end
 
-    def map(text)
-      @data.detect do |term, mapping|
-        !!mapping.detect { |str| str == text }
+    def reduce(text)
+      tokens = tokenize(text)
+      return unless term = @data.detect do |term, mapping|
+        mapping.detect do |str|
+          term_tokens = tokenize(str)
+          next unless offset = tokens.index { |t| t == term_tokens.first }
+          last_term = offset + (term_tokens.size - 1)
+          next unless tokens[offset..last_term].map(&:downcase) == term_tokens.map(&:downcase)
+          tokens.slice!(offset..last_term)
+          true
+        end
       end.try(:first)
+
+      { term: term, tokens: tokens }
     end
 
+    def has_term?(term); @data.has_key?(term.to_s); end
     def terms; @data.keys; end
     def [](term); @data[term.to_s]; end
+
+    #private
+
+    def tokenize(str)
+      tokenizer.tokenize(str).reject { |t| t.empty? }
+    end
+
+    def untokenize(tokens)
+      str = ""
+      tokens.each { |t| str << "#{t} " }
+      str.strip
+    end
   end
 end
