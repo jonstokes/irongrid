b96db4f5fc99645941b4f22c33d520e0156b0dd7
Jon Stokes <jon@paperhatpress.com>
2014-09-18 12:07:42 -0500

    Stubbed in code for multiple caliber dictionaries

--- spec/fixtures/stretched/registrations/globals/scripts/product_page.rb
+++ spec/fixtures/stretched/registrations/globals/scripts/product_page.rb
@@ -79,10 +79,21 @@ Stretched::Script.define do
     end
 
     product_caliber do |instance|
-      mapping = Stretched::Mapping.find("calibers")
-      extract_metadata(:product_caliber, mapping, instance) ||
-        extract_metadata(:title, mapping, instance) ||
-        extract_metadata(:keywords, mapping, instance)
+      %w(rimfire_calibers pistol_calibers shotgun_calibers rifle_calibers).detect do |mapping_name|
+        mapping = Stretched::Mapping.find(mapping_name)
+        extract_metadata(:product_caliber, mapping, instance) ||
+          extract_metadata(:title, mapping, instance) ||
+          extract_metadata(:keywords, mapping, instance)
+      end
+    end
+
+    product_caliber_category do |instance|
+      if instance.product_caliber
+        %w(rimfire_calibers pistol_calibers shotgun_calibers rifle_calibers).detect do |mapping_name|
+          mapping = Stretched::Mapping.find(mapping_name)
+          mapping_name.name.split("_calibers").first if mapping.terms.include?(instance.product_caliber)
+        end
+      end
     end
 
     product_manufacturer do |instance|
