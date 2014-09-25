060e9e992fd1fd53bd69c145510c6d63f89e4942
Jon Stokes <jon@paperhatpress.com>
2014-09-25 18:11:54 -0500

    Fixed stretched gem init. Untested

--- config/initializers/stretched.rb
+++ config/initializers/stretched.rb
@@ -1,5 +1,9 @@
 user = Figaro.env.stretched_user rescue "#{Rails.env}@ironsights.com"
+pool = Figaro.env.redis_pool.to_i rescue 10
 
 Stretched::Settings.configure do |config|
   config.user = user
+  config.redis_pool_size = pool
+  config.redis_url = Figaro.env.stretched_redis_url
 end
+
