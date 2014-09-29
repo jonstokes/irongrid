8725a0851b04fb0e305232c3098deaed978ca075
Jon Stokes <jon@paperhatpress.com>
2014-09-29 17:45:53 -0500

    Removed more unused code

--- lib/modules/product_details/product_details.rb
+++
@@ -1,9 +0,0 @@
-module ProductDetails
-  def self.renormalize_all(text)
-    return unless text.present?
-    str = text.dup
-    str = Caliber.parse(str)[:text]
-    str = Manufacturer.parse(str)[:text]
-    str
-  end
-end
