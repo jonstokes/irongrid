14ce71984d68bb5ed0034d018a604be6bd3c7004
Jon Stokes <jon@paperhatpress.com>
2014-10-14 12:19:38 -0500

    Fix sidekiq init

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
-  config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => ENV['RAILS_ENV'] }
+  config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => Rails.env }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
   end
@@ -12,7 +12,7 @@ end
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
   config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
-  config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => ENV['RAILS_ENV'] }
+  config.redis = { :url => Figaro.env.sidekiq_redis_url, :namespace => Rails.env }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
   end
