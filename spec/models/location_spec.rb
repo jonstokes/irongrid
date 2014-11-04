e43bc5a563214b033a8d49d7749d05cddaab7c17
Jon Stokes <jon@jonstokes.com>
2014-11-04 15:56:30 -0600

    Location specs are green again

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -16,19 +16,26 @@ describe Location do
     @key = "1213 Newning Ave., Austin TX"
   end
 
-  describe "self.put", no_es: true do
+  before :each do
+    Location.put_mapping
+  end
+
+  describe "self.put" do
     it "should write a new Location object to the table" do
       Location.put(@key).should_not be_nil
+      Location.refresh_index
       Location.count.should == 1
       loc = Location.first
-      loc.latitude.should == 30.25054399999999
-      loc.longitude.should == -97.74310799999999
+      loc.latitude.should == '30.25054399999999'
+      loc.longitude.should == '-97.74310799999999'
     end
 
     it "should return the object if the key already exists" do
       loc1 = Location.put(@key)
+      Location.refresh_index
       loc1.should_not be_nil
       loc2 = Location.put(@key)
+      Location.refresh_index
       Location.count.should == 1
       loc2.should_not be_nil
       loc1.id.should == loc2.id
@@ -36,12 +43,14 @@ describe Location do
 
     it "should return a copy of the new object" do
       loc = Location.put(@key)
+      Location.refresh_index
       loc.is_a?(Location).should be_true
       loc.id.should_not be_nil
     end
 
     it "should not be case sensitive with keys" do
       Location.put(@key.downcase).id.should == Location.put(@key.upcase).id
+      Location.refresh_index
       Location.count.should == 1
     end
 
@@ -54,16 +63,17 @@ describe Location do
     end
   end
 
-  describe "fetch_data", no_es: true do
+  describe "fetch_data" do
     it "should populate the Location object's data field with the geolocation data" do
       loc = Location.create(:id => @key)
+      Location.refresh_index
       loc.fetch_data
       loc.city.should == "Austin"
       loc.state.should == "Texas"
       loc.state_code.should == "TX"
       loc.postal_code.should == "78704"
-      loc.latitude.should == 30.25054399999999
-      loc.longitude.should == -97.74310799999999
+      loc.latitude.should == "30.25054399999999"
+      loc.longitude.should == "-97.74310799999999"
     end
   end
 end
