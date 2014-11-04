a53b8b7d5b9ae9a8bb8c3c73fe27c516a54e6f4c
Jon Stokes <jon@jonstokes.com>
2014-11-04 16:36:08 -0600

    TRackable module specs are green now

--- lib/modules/trackable.rb
+++ lib/modules/trackable.rb
@@ -6,10 +6,10 @@ module Trackable
     return if @log_record_schema # Ignore repeated calls to #track, as in RefreshLinksWorker
     opts.symbolize_keys!
     @log_record_schema = self.class::LOG_RECORD_SCHEMA
-    @record = { data: {} }
     @write_interval = opts[:write_interval] || 500
     @count = 0
     @tracking = true
+    initialize_log_record
     status_update(true)
   end
 
@@ -41,6 +41,30 @@ module Trackable
   end
 
   private
+
+  def initialize_log_record
+    @record ||= {}
+    @record[:data] ||= {}
+    data = {}
+
+    self.class::LOG_RECORD_SCHEMA.each do |k, v|
+      if v == Integer
+        data[k] = 0
+      else
+        data[k] = v.new
+      end
+    end
+
+    data.merge!(domain: @domain, complete: false)
+
+    @record.merge!(
+        jid:   self.jid,
+        agent: "#{self.class}",
+        archived: false
+    )
+    @record[:data].merge!(data)
+  end
+
   def log_record_attributes
     @log_record_schema.keys + [:jid, :agent, :archived]
   end
