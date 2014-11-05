89c506373d93f296ef4fb898136c15aa0f577cc8
Jon Stokes <jon@jonstokes.com>
2014-11-05 15:08:51 -0600

    CopyAttributes interactor now uses a mapping file

--- app/interactors/copy_attributes.rb
+++ app/interactors/copy_attributes.rb
@@ -1,46 +1,34 @@
 class CopyAttributes
   include Interactor
 
-  def perform
-    copy_directly
-    copy_and_rename
+  def call
+    context.listing = Hashie::Mash.new
+    map_json(
+        context.listing_json,
+        context.listing,
+        json_mapping
+    )
+    context.message1 = context.listing_json.message1
+    context.message2 = context.listing_json.message2
+    context.message3 = context.listing_json.message3
+    context.message4 = context.listing_json.message4
   end
 
-  def copy_directly
-    %w(
-      url
-      type
-      keywords
-      description
-      weight_in_pounds
-      discount_in_cents
-      discount_percent
-      current_price_in_cents
-      price_in_cents
-      sale_price_in_cents
-      price_on_request
-      buy_now_price_in_cents
-      current_bid_in_cents
-      minimum_bid_in_cents
-      reserve_in_cents
-      message1
-      message2
-      message3
-      message4
-      message5
-    ).map(&:to_sym).each do |key|
-      context[key] = listing_json[key]
+  def json_mapping
+    @json_to_es_mapping ||= Hashie::Mash.new YAML.load_file "#{Rails.root}/lib/object_mappings/listing.yml"
+  end
+
+  def map_json(json, listing, mapping)
+    mapping.each do |key, value|
+      if value.is_a?(Hashie::Mash)
+        field = Hashie::Mash.new
+        map_json(json, field, value)
+        listing[key] = field unless field.empty?
+      else
+        next unless json[value]
+        listing[key] = json[value]
+      end
     end
-  end
-
-  def copy_and_rename
-    context[:availability]       = listing_json.availability || "Unknown"
-    context[:item_condition]     = listing_json.condition || "Unknown"
-    context[:image_source]       = listing_json.image
-    context[:item_location]      = listing_json.location
-    context[:upc]                = listing_json.product_upc
-    context[:mpn]                = listing_json.product_mpn
-    context[:sku]                = listing_json.product_sku
   end
 end
 
