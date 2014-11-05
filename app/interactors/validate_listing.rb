c75937f64f033e6bce5183170fd7f28a5efac6cc
Jon Stokes <jon@jonstokes.com>
2014-11-05 16:53:29 -0600

    More changes to interactors

--- app/interactors/validate_listing.rb
+++ app/interactors/validate_listing.rb
@@ -2,17 +2,17 @@ class ValidateListing
   include Interactor
 
   def perform
-    context.fail!(status: :not_found) if (listing_json.type == "AuctionListing") && auction_ended?
+    context.fail!(status: :not_found) if (context.listing_json.type == "AuctionListing") && auction_ended?
     context.fail!(status: :invalid) if success? && !is_valid?
     context[:listing] = {} unless success?
   end
 
   def is_valid?
-    listing_json.valid?
+    context.listing_json.valid?
   end
 
   def auction_ended?
-    auction_ends.nil? || (auction_ends < Time.now)
+    context.listing.auction_ends.nil? || (context.listing.auction_ends < Time.now.utc)
   end
 
 end
--- app/interactors/validate_listing_presence.rb
+++ app/interactors/validate_listing_presence.rb
@@ -2,13 +2,21 @@ class ValidateListingPresence
   include Interactor
 
   def perform
-    context[:listing] = nil
-    context[:listing_json] = context[:object]
-    context[:url] = listing_json.try(:url)
+    context.listing = nil
+    context.listing_json = context.object
+    context.url = context.listing_json.try(:url)
     context.fail!(status: :not_found) if not_found?
   end
 
   def not_found?
     !page.fetched? || page.error || !page.body? || page.code.nil? || (page.code.to_i == 404) || listing_json.try(:not_found?)
+  end
+
+  def page
+    context.page
+  end
+
+  def listing_json
+    context.listing_json
   end
 end
