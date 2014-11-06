b7138f0a4f083367289d1b726b12faf2eed5fc6d
Jon Stokes <jon@jonstokes.com>
2014-11-06 12:26:22 -0600

    More green shipping specs

--- spec/interactors/run_loadable_scripts_spec.rb
+++ spec/interactors/run_loadable_scripts_spec.rb
@@ -79,6 +79,7 @@ describe RunLoadableScripts do
 
     it 'sets the shipping cost using a script' do
       listing = IronBase::Listing.new(
+          price: { current: 100 },
           product: { category1: 'Guns' },
       )
       results = RunLoadableScripts.call(
@@ -87,9 +88,14 @@ describe RunLoadableScripts do
           listing_json: Hashie::Mash.new
       )
       expect(results.listing.shipping_cost).to eq(0)
+      expect(results.listing.with_shipping.price.current).to eq(100)
 
       listing = IronBase::Listing.new(
-          product: { category1: 'Ammunition' },
+          price: { current: 100 },
+          product: {
+              category1: 'Ammunition',
+              number_of_rounds: 100
+          }
       )
       results = RunLoadableScripts.call(
           listing: listing,
@@ -97,6 +103,8 @@ describe RunLoadableScripts do
           listing_json: Hashie::Mash.new
       )
       expect(results.listing.shipping_cost).to eq(995)
+      expect(results.listing.with_shipping.price.current).to eq(1095)
+      expect(results.listing.with_shipping.price.per_round).to eq(11)
 
       listing = IronBase::Listing.new(
           product: { category1: 'Optics' },
@@ -107,6 +115,34 @@ describe RunLoadableScripts do
           listing_json: Hashie::Mash.new
       )
       expect(results.listing.shipping_cost).to be_nil
+      expect(results.listing.with_shipping).to be_nil
     end
+
+    it 'does not blow up if the shipping makes the discount to be zero' do
+      listing = IronBase::Listing.new(
+          product: {
+              category1: 'Ammunition',
+              number_of_rounds: 100
+          },
+          price: {
+              current: 90,
+              sale: 90,
+              list: 100
+          },
+          discount: {
+              in_cents: 10,
+              percent: 10
+          },
+          shipping_cost: 10
+      )
+      results = RunLoadableScripts.call(
+          listing: listing,
+          site: @site,
+          listing_json: Hashie::Mash.new
+      )
+      expect(results.listing.with_shipping.discount.in_cents).to eq(0)
+
+    end
+
   end
 end
