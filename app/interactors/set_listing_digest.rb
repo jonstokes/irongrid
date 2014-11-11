74d3a22c167c47d218665f736e4e827813d722bc
Jon Stokes <jon@jonstokes.com>
2014-11-11 10:38:00 -0600

    Working on digest interactor

--- app/interactors/set_listing_digest.rb
+++ app/interactors/set_listing_digest.rb
@@ -1,60 +1,10 @@
 class SetListingDigest
   include Interactor
 
-  DEFAULT_DIGEST_ATTRIBUTES = %w(
-    title
-    keywords
-    image.source
-    type
-    seller.domain
-    condition
-    location.source
-    product.category1
-    product.caliber_category
-    product.caliber
-    product.manufacturer
-    product.grains
-    product.number_of_rounds
-    price.current
-  )
-
   def call
-    context.listing.digest = digest
+    context.listing.digest_attributes = context.site.digest_attributes
+    context.listing.set_digest!
     context.fail(status: :duplicate) if context.listing.digest_is_duplicate?
-  end
-
-  def digest
-    digest_string = ''
-    get_digest_attributes(default_digest_attributes).each do |attr|
-      next unless value = get_value(attr)
-      digest_string << "#{value}"
-    end
-    Digest::MD5.hexdigest(digest_string)
-  end
-
-  def get_value(attr)
-    instance_eval("context.listing.#{attr}")
-  rescue
-    nil
-  end
-
-  def get_digest_attributes(defaults)
-    return defaults unless attrs = context.site.digest_attributes
-    return attrs unless attrs.include?("defaults")
-    attrs = defaults + attrs # order matters here, so no +=
-    attrs.delete("defaults")
-    attrs
-  end
-
-  def default_digest_attributes
-    case context.listing.type
-    when "AuctionListing"
-      DEFAULT_DIGEST_ATTRIBUTES + %w(auction_ends)
-    when "RetailListing"
-      DEFAULT_DIGEST_ATTRIBUTES + %w(price.list price.sale price.on_request availability)
-    when "ClassifiedListing"
-      DEFAULT_DIGEST_ATTRIBUTES + %w(price.sale price.list)
-    end
   end
 end
 
