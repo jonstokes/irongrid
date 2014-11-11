5e811491e100b732b13654ceaa76dab3126dc039
Jon Stokes <jon@jonstokes.com>
2014-11-11 08:42:46 -0600

    Reworking PLW specs

--- spec/workers/pull_listings_worker_spec.rb
+++ spec/workers/pull_listings_worker_spec.rb
@@ -8,6 +8,7 @@ describe PullListingsWorker do
   before :each do
     # IronGrid
     @site = create_site "www.retailer.com"
+    @not_found_redirect = "http://#{@site.domain}/"
     LinkMessageQueue.new(domain: @site.domain).clear
     ImageQueue.new(domain: @site.domain).clear
     CDN.clear!
@@ -44,17 +45,18 @@ describe PullListingsWorker do
       object: @listing_json
     }
     @object_q = Stretched::ObjectQueue.new("#{@site.domain}/listings")
+
+    @listing = build(:listing, :retail)
+    @listing_json = Mapper.new.reverse_map(@listing).to_hash.merge(valid: true)
+    @listing_data = @listing.data.merge(id: @listing.id)
   end
 
   describe '#perform' do
     describe 'Existing listing' do
       it 'updates a listing with new attributes' do
-        existing_listing = create(:listing, :retail)
+        existing_listing = IronBase::Listing.create(@listing_data)
         page = @page.merge(url: existing_listing.url.page)
-        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(
-            title: 'Updated Listing',
-            valid: true
-        ).to_hash
+        new_listing_json = @listing_json.merge(title: 'Updated Listing')
 
         @object_q.add @object.merge(
             object: new_listing_json,
@@ -72,17 +74,16 @@ describe PullListingsWorker do
       end
 
       it 'updates a feed listing with new attributes' do
-        existing_listing = create(:listing, :retail)
+        existing_listing = IronBase::Listing.create(@listing_data)
         existing_listing.url.page = 'http://www.retailer.com/feed.xml'
         existing_listing.save
 
         page = @page.merge(url: 'http://www.retailer.com/feed.xml')
 
-        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(
+        new_listing_json = @listing_json.merge(
             title: 'Updated Listing',
             url: existing_listing.url.purchase,
-            valid: true
-        ).to_hash
+        )
 
         @object_q.add @object.merge(
                           object: new_listing_json,
@@ -101,17 +102,17 @@ describe PullListingsWorker do
       end
 
       it 'deactivates an invalid feed listing' do
-        existing_listing = create(:listing, :retail)
+        existing_listing = IronBase::Listing.create(@listing_data)
         existing_listing.url.page = 'http://www.retailer.com/feed.xml'
         existing_listing.save
 
         page = @page.merge(url: 'http://www.retailer.com/feed.xml')
 
-        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(
+        new_listing_json = @listing_json.merge(
             title: 'Updated Listing',
             url: existing_listing.url.purchase,
             valid: false
-        ).to_hash
+        )
 
         @object_q.add @object.merge(
                           object: new_listing_json,
@@ -131,195 +132,169 @@ describe PullListingsWorker do
       end
 
       it 'deactivates an invalid retail listing' do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
-        page = @page.merge('url' => existing_listing.url)
+        existing_listing = IronBase::Listing.create(@listing_data)
+        page = @page.merge(url: existing_listing.url)
+        new_listing_json = @listing_json.merge(valid: false)
 
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'invalid'
-        )
-        listing = Listing.first
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+        listing = IronBase::Listing.first
+
+        expect(listing.inactive?).to be_true
         expect(listing.digest).to eq(existing_listing.digest)
-        expect(listing.url).to eq(existing_listing.url)
-        expect(listing.update_count).to eq(1)
+        expect(listing.url.purchase).to eq(existing_listing.url.purchase)
+        expect(listing.url.page).to eq(existing_listing.url.page)
+        expect(listing.id).to eq(existing_listing.id)
         expect(updated_today?(listing)).to be_true
-        expect(listing.active?).to be_false
       end
 
-      it "deletes a listing that redirects to an invalid page" do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
+      it 'deletes a listing that redirects to an invalid page' do
+        existing_listing = create(:listing, :retail)
         page = @page.merge(
-            'url'           => @not_found_redirect,
-            'redirect_from' => existing_listing.url,
-            'code'          => 301
+            url: @not_found_redirect,
+            redirect_from: existing_listing.url,
+            code: 301
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'invalid'
-        )
-        expect(Listing.count).to eq(0)
+        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(valid: false).to_hash
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+        expect(IronBase::Listing.search).to be_empty
       end
 
       it "deletes a listing that redirects to a not_found page" do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
+        existing_listing = create(:listing, :retail)
         page = @page.merge(
-            'url'           => @not_found_redirect,
-            'redirect_from' => existing_listing.url,
-            'code'          => 301
+            url: @not_found_redirect,
+            redirect_from: existing_listing.url,
+            code: 301
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'not_found'
-        )
-        expect(Listing.count).to eq(0)
+        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(not_found: true).to_hash
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+        expect(IronBase::Listing.search).to be_empty
       end
 
       it "updates a listing that 301 moved permanently with a new url" do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
+        existing_listing = create(:listing, :retail)
+        redirect_url = "#{existing_listing.url.page}123"
         page = @page.merge(
-            'url'           => @redirect_url,
-            'redirect_from' => existing_listing.url,
-            'code'          => 301
+            url: redirect_url,
+            redirect_from: existing_listing.url,
+            code: 301
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'success'
-        )
-        expect(Listing.count).to eq(1)
-        listing = Listing.first
-        expect(listing.digest).to eq("bbbb")
-        expect(listing.url).to eq(@redirect_url)
-        expect(listing.update_count).to eq(1)
+        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(valid: true).to_hash
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+
+        listing = IronBase::Listing.first
+        expect(listing.url.page).to eq(redirect_url)
+        expect(listing.digest).to eq(existing_listing.digest)
         expect(updated_today?(listing)).to be_true
       end
 
       it "updates a listing that 302 moved temporarily, but keeps original url" do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
+        existing_listing = create(:listing, :retail)
+        redirect_url = "#{existing_listing.url.page}123"
         page = @page.merge(
-            'url'           => @redirect_url,
-            'redirect_from' => existing_listing.url,
-            'code'          => 302
+            url: redirect_url,
+            redirect_from: existing_listing.url,
+            code: 302
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'success'
-        )
-        expect(Listing.count).to eq(1)
-        listing = Listing.first
-        expect(listing.digest).to eq("bbbb")
-        expect(listing.url).to eq(existing_listing.url)
-        expect(listing.update_count).to eq(1)
+        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(valid: true).to_hash
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+
+        listing = IronBase::Listing.first
+        expect(listing.url.page).to eq(existing_listing.url.page)
+        expect(listing.digest).to eq(existing_listing.digest)
         expect(updated_today?(listing)).to be_true
       end
 
       it "deletes a listing that 404s" do
-        existing_listing = FactoryGirl.create(:retail_listing, :stale)
+        existing_listing = create(:listing, :retail)
         page = @page.merge(
-            'url'           => existing_listing.url,
-            'code'          => 404
+            url:  existing_listing.url,
+            code: 404
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => "bbbb"),
-            status:  'not_found'
-        )
-        expect(Listing.count).to eq(0)
+        new_listing_json = Mapper.new.reverse_map(existing_listing).merge(valid: true).to_hash
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        @worker.perform(domain: @site.domain)
+        IronBase::Listing.refresh_index
+        expect(IronBase::Listing.search).to be_empty
       end
 
       it "deletes a listing that is discovered to be a duplicate" do
         # A retail listing is created in the database
-        listing_v1 = FactoryGirl.create(:retail_listing, :stale)
+        listing_v1 = create(:listing, :retail)
 
         # Later, that same listing moves (HTTP 301) to a new url and goes on sale,
         # so that the url, price, an digest are all different. This platform will
         # therefore think this is a new listing, although it's really an updated
         # version of listing_v1.
-        listing_v2 = FactoryGirl.create(:retail_listing)
+        listing_v2 = create(:listing, :retail)
 
         # Now when we try to refresh listing_v1, the platform will realize that
         # it has a dupe because the new url & digest for the refreshed listing_v1
         # already exists in the database as listing_v2. Therefore
         # we need to delete listing_v1.
         page = @page.merge(
-            'url'           => listing_v2.url,
-            'redirect_from' => listing_v1.url,
-            'code'          => 301
+            url:           listing_v2.url,
+            redirect_from: listing_v1.url,
+            code:           301
         )
-        WriteListingWorker.new.perform(
-            page:    page,
-            listing: @listing_data.merge("digest" => listing_v2.digest),
-            status:  'success'
-        )
-        expect(Listing.count).to eq(1)
-        expect { Listing.find(listing_v1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+
+        new_listing_json = Mapper.new.reverse_map(listing_v2).merge(
+            valid: true,
+        ).to_hash
+
+        @object_q.add @object.merge(
+                          object: new_listing_json,
+                          page:   page
+                      )
+
+        expect(IronBase::Listing.search.size).to eq(1)
+        expect(Listing.find(listing_v1.id)).to be_nil
         listing = Listing.find(listing_v2.id)
 
-        expect(listing.url).to eq(page['url'])
+        expect(listing.url).to eq(page[:url])
         expect(listing.digest).to eq(listing_v2.digest)
         expect(updated_today?(listing)).to be_true
       end
-
     end
 
     it "pops objects from the ObjectQueue for the domain" do
       @object_q.add(@object)
       @worker.perform(domain: @site.domain)
       expect(@object_q.size).to eq(0)
-    end
-
-    it "correctly tags a 404 link" do
-      @object[:page].merge!(body: false, code: 404)
-      @object_q.add(@object)
-      @worker.perform(domain: @site.domain)
-      expect(WriteListingWorker._jobs.count).to eq(1)
-      job = WriteListingWorker._jobs.first
-      args = Hashie::Mash.new job["args"].first
-      expect(args.status).to eq('not_found')
-      expect(args.page.code).to eq(404)
-    end
-
-    it "correctly tags a not_found link in redis" do
-      @object[:object] = {
-        "seller_domain" => @site.domain,
-        "not_found"     => true,
-        "title"         => "Title"
-      }
-      @object_q.add(@object)
-      @worker.perform(domain: @site.domain)
-      expect(WriteListingWorker._jobs.count).to eq(1)
-      job = WriteListingWorker._jobs.first
-      args = Hashie::Mash.new job["args"].first
-      expect(args.status).to eq('not_found')
-    end
-
-    it "correctly tags an invalid link in redis" do
-      @object[:object] = {
-        "seller_domain" => @site.domain,
-        "not_found"     => false,
-        "valid"         => false,
-        "title"         => "Title"
-      }
-      @object_q.add(@object)
-      @worker.perform(domain: @site.domain)
-      expect(WriteListingWorker._jobs.count).to eq(1)
-      job = WriteListingWorker._jobs.first
-      args = Hashie::Mash.new job["args"].first
-      expect(args.status).to eq('invalid')
-    end
-
-    it "correctly tags a valid link in redis" do
-      @object_q.add(@object)
-      @worker.perform(domain: @site.domain)
-      expect(WriteListingWorker._jobs.count).to eq(1)
-      job = WriteListingWorker._jobs.first
-      args = Hashie::Mash.new job["args"].first
-      expect(args.status).to eq('success')
-      expect(args.listing["digest"]).to eq("862e3a129f9da0c4a4ffdef2d4a6cb09")
     end
 
     describe "write to listings table from a generic full product feed" do
