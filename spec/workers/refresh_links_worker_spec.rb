530e7e01c85c9b94c451bfcd5617ec36d5bdfb45
Jon Stokes <jon@jonstokes.com>
2014-11-12 12:50:48 -0600

    Some spec fixes

--- spec/workers/refresh_links_worker_spec.rb
+++ spec/workers/refresh_links_worker_spec.rb
@@ -23,13 +23,14 @@ describe RefreshLinksWorker do
       stale_listing = FactoryGirl.create(:listing, updated_at: Time.now - 5.days)
       5.times { FactoryGirl.create(:listing, updated_at: Time.now - 5.days) }
       FactoryGirl.create(:listing, updated_at: Time.now)
+      IronBase::Listing.refresh_index
       RefreshLinksWorker.new.perform(domain: @site.domain)
       msg = LinkMessageQueue.find(stale_listing.url.page)
       expect(msg.listing_id).to eq(stale_listing.id)
       expect(msg.listing_digest).to eq(stale_listing.digest)
 
       lq = LinkMessageQueue.new(domain: "www.retailer.com")
-      lq.size.should == 6
+      expect(@site.link_message_queue.size).to eq(6)
       expect(lq.pop.url).to match(/retailer\.com/)
     end
   end
