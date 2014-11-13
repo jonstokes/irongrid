5737050f835dce5baeb12997ad420ac409b15c5d
Jon Stokes <jon@jonstokes.com>
2014-11-13 12:15:59 -0600

    Fixed Find Or Create Listing interactor spec

--- spec/interactors/find_or_create_listing_spec.rb
+++ spec/interactors/find_or_create_listing_spec.rb
@@ -1,47 +1,80 @@
 require 'spec_helper'
 
 describe FindOrCreateListing do
-  before :each do
-    @listing_json = Hashie::Mash.new attributes_for(:listing)
-    @url = @listing_json.url
-    @listing_json.url = @listing_json.id = nil
-    @page = Hashie::Mash.new(code: 200)
-    @id_tag = 'sku-1234'
-  end
-
   describe '#call' do
     it 'retrieves a listing object for a listing that already exists in the index' do
       listing = create(:listing)
+      listing_json = Hashie::Mash.new(ObjectMapper.json_from_listing(listing))
+      page = Hashie::Mash.new(
+          code: 200,
+          url: listing.url.page
+      )
       IronBase::Listing.refresh_index
-      listing2 = FindOrCreateListing.call(listing_json: listing.data, url: listing.url).listing
+      listing2 = FindOrCreateListing.call(
+          listing_json: listing_json,
+          page:         page,
+          url:          listing.url
+      ).listing
       expect(listing2.persisted?).to eq(true)
       expect(IronBase::Listing.count).to eq(1)
       expect(IronBase::Listing.find(listing2.id).id).to eq(listing.id)
     end
 
     it 'returns a new, unsaved listing object when a listing with this id does not exist in the index' do
-      listing = create(:listing)
+      listing_json = Hashie::Mash.new(
+          title: 'New Listing'
+      )
+      page = Hashie::Mash.new(
+          code: 200,
+          url: 'http://www.retailer.com/1'
+      )
       IronBase::Listing.refresh_index
-      listing2 = FindOrCreateListing.call(
-          listing_json: listing.data,
-          url: listing.url.merge(purchase: "#{@url}-123")
+      listing = FindOrCreateListing.call(
+          listing_json: listing_json,
+          url: Hashie::Mash.new(page: page.url, purchase: page.url),
+          page: page
       ).listing
-      expect(listing2.persisted?).to eq(false)
-      expect(IronBase::Listing.count).to eq(1)
-      expect(IronBase::Listing.find(listing2.id)).to be_nil
+      expect(listing.persisted?).to eq(false)
+      expect(IronBase::Listing.count).to eq(0)
+      expect(IronBase::Listing.find(listing.id)).to be_nil
     end
   end
 
   describe '#listing_id' do
     it 'should use the purchase url when there is no id tag' do
-      listing = FindOrCreateListing.call(listing_json: @listing_json, url: @url).listing
-      expect(listing.id).to eq(@url.purchase)
+      listing_json = Hashie::Mash.new(title: 'New Listing')
+      page_url = 'http://www.retailer.com'
+      purchase_url = "#{page_url}-buy"
+      page = Hashie::Mash.new(
+          code: 200,
+          url: page_url
+      )
+      listing = FindOrCreateListing.call(
+          listing_json: listing_json,
+          url: Hashie::Mash.new(page: page_url, purchase: purchase_url),
+          page: page
+      ).listing
+      expect(listing.id).to eq(purchase_url)
     end
 
     it 'should use an id tagged url when there is an id tag in the listing json' do
-      listing_json = @listing_json.merge(id: @id_tag)
-      listing = FindOrCreateListing.call(listing_json: listing_json, url: @url).listing
-      expect(listing.id).to eq("#{@url.purchase}!#{@id_tag}")
+      id_tag = '-123'
+      listing_json = Hashie::Mash.new(
+          title: 'New Listing',
+          id: id_tag
+      )
+      page_url = 'http://www.retailer.com'
+      purchase_url = "#{page_url}-buy"
+      page = Hashie::Mash.new(
+          code: 200,
+          url: page_url
+      )
+      listing = FindOrCreateListing.call(
+          listing_json: listing_json,
+          url: Hashie::Mash.new(page: page_url, purchase: purchase_url),
+          page: page
+      ).listing
+      expect(listing.id).to eq("#{purchase_url}!#{id_tag}")
     end
   end
 end
