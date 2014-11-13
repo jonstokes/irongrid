e8a1fa12da6fa514454d86e11b16dbd2a5fc873d
Jon Stokes <jon@jonstokes.com>
2014-11-13 10:18:14 -0600

    Site stats are working. All workers should be green

--- spec/workers/site_stats_worker_spec.rb
+++ spec/workers/site_stats_worker_spec.rb
@@ -4,17 +4,18 @@ describe SiteStatsWorker do
   describe "#perform" do
     it "generates site stats for a site" do
       site = create_site "www.retailer.com"
-      FactoryGirl.create(:listing)
+      create(:listing)
       stalest = FactoryGirl.create(:listing, updated_at: Time.now - 10.days)
-      FactoryGirl.create(:listing, :inactive)
+      create(:listing, :inactive)
+      IronBase::Listing.refresh_index
 
       worker = SiteStatsWorker.new
       worker.perform(domain: site.domain)
 
       s = Site.new(domain: site.domain, source: :redis)
-      expect(s.site_data[:stats][:active_listings]).to eq(2)
       expect(s.site_data[:stats][:inactive_listings]).to eq(1)
-      expect(s.site_data[:stats][:stalest_listing].strftime("%Y-%m-%dT%H:%M:%S")).to eq(stalest.updated_at)
+      expect(s.site_data[:stats][:active_listings]).to eq(2)
+      expect(s.site_data[:stats][:stalest_listing].strftime("%Y-%m-%dT%H:%M:%S")).to eq(stalest.updated_at.strftime("%Y-%m-%dT%H:%M:%S"))
       expect(s.site_data[:stats][:updated_at]).to be_a(Time)
 
     end
