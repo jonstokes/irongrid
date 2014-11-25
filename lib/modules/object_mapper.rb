d54417ec4dbce2bc85fb757347b2fef4526d549d
Jon Stokes <jon@jonstokes.com>
2014-11-25 09:20:24 -0600

    Cleaned up reverse mapping

--- lib/modules/object_mapper.rb
+++ lib/modules/object_mapper.rb
@@ -12,8 +12,8 @@ module ObjectMapper
         transform(nopts)
         listing[key] = field unless field.empty?
       else
-        next unless json.send(value)
-        listing[key] = json.send(value)
+        next unless json[value]
+        listing[key] = json[value]
       end
     end
   end
@@ -33,31 +33,31 @@ module ObjectMapper
       json[attr] = listing[attr]
     end
 
-    json.url = listing.url.purchase
+    json.url = listing['url']['purchase']
     json.shipping_cost_in_cents = listing.shipping_cost
-    json.image = listing.image.source
-    json.location = listing.location.try(:source)
-    json.discount_in_cents = listing.discount.try(:in_cents)
-    json.discount_percent = listing.discount.try(:percent)
-    json.price_on_request = listing.price.try(:on_request)
-    json.current_price_in_cents = listing.price.try(:current)
-    json.price_in_cents = listing.price.try(:list)
-    json.sale_price_in_cents = listing.price.try(:sale)
-    json.buy_now_price_in_cents = listing.price.try(:buy_now)
-    json.current_bid_in_cents = listing.price.try(:current_bid)
-    json.minimum_bid_in_cents = listing.price.try(:minimum_bid)
-    json.reserve_in_cents = listing.price.try(:reserve)
-    json.price_per_round_in_cents = listing.price.try(:per_round)
-    json.product_upc = listing.product.upc
-    json.product_sku = listing.product.sku
-    json.product_mpn = listing.product.mpn
-    json.product_category1 = listing.product.category1
-    json.product_manufacturer = listing.product.manufacturer
-    json.weight_in_pounds = listing.product.shipping
-    json.product_caliber = listing.product.caliber
-    json.product_caliber_category = listing.product.caliber_category
-    json.product_number_of_rounds = listing.product.number_of_rounds
-    json.product_grains = listing.product.grains
+    json.image = listing['image']['source']
+    json.location = listing['location']['source'] rescue nil
+    json.discount_in_cents = listing['discount']['in_cents'] rescue nil
+    json.discount_percent = listing['discount']['percent'] rescue nil
+    json.price_on_request = listing['price']['on_request'] rescue nil
+    json.current_price_in_cents = listing['price']['current'] rescue nil
+    json.price_in_cents = listing['price']['list'] rescue nil
+    json.sale_price_in_cents = listing['price']['sale'] rescue nil
+    json.buy_now_price_in_cents = listing['price']['buy_now'] rescue nil
+    json.current_bid_in_cents = listing['price']['current_bid'] rescue nil
+    json.minimum_bid_in_cents = listing['price']['minimum_bid'] rescue nil
+    json.reserve_in_cents = listing['price']['reserve'] rescue nil
+    json.price_per_round_in_cents = listing['price']['per_round'] rescue nil
+    json.product_upc = listing['product']['upc']
+    json.product_sku = listing['product']['sku']
+    json.product_mpn = listing['product']['mpn']
+    json.product_category1 = listing['product']['category1']
+    json.product_manufacturer = listing['product']['manufacturer']
+    json.weight_in_pounds = listing['product']['shipping']
+    json.product_caliber = listing['product']['caliber']
+    json.product_caliber_category = listing['product']['caliber_category']
+    json.product_number_of_rounds = listing['product']['number_of_rounds']
+    json.product_grains = listing['product']['grains']
     json
   end
 
