fbcc94d36290bd7b361cf3e26b23752c16e8bf14
Jon Stokes <jon@jonstokes.com>
2014-11-29 18:32:26 -0600

    Some fixes

--- app/interactors/merge_json_into_product.rb
+++ app/interactors/merge_json_into_product.rb
@@ -5,7 +5,9 @@ class MergeJsonIntoProduct
     return if context.product.complete?
     context.product.mpn = context.product_json.mpn
     context.product.sku = context.product_json.sku
-    IronBase::Product.properties.except(:mpn, :sku).each do |attr|
+    context.product.weight ||= { shipping: context.product_json.weight }
+    context.product.image ||= { source: context.product_json.image_source}
+    IronBase::Product.properties.except(:mpn, :sku, :weight, :image).each do |attr|
       context.product[attr] ||= context.product_json[attr]
     end
     context.product.save
