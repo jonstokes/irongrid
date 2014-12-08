0d0838f1c8d9b00617b20b04a2900ca4ceaae0a1
Jon Stokes <jon@jonstokes.com>
2014-12-08 14:02:09 -0600

    Squash

---
+++ spec/models/listing_migration_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ListingMigration do
+  describe 'page_url' do
+    it 'gives the feed url for a feed listing' do
+      pending "Example"
+    end
+
+    it 'gives the bare_url for a non-feed listing' do
+      pending "Example"
+    end
+  end
+
+  describe 'listing_url' do
+    it 'gives the bare_url for a feed listing' do
+      pending "EXample"
+    end
+
+    it 'gives nil for a non-feed listing' do
+      pending "Example"
+    end
+  end
+end
