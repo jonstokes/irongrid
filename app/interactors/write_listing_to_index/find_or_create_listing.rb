fe45e8bb4cdc1b4feabdc2345cb00228703c77bb
Jon Stokes <jon@jonstokes.com>
2014-12-10 10:16:48 -0600

    Move all seturl code over to find-or-create url

--- app/interactors/write_listing_to_index/find_or_create_listing.rb
+++ app/interactors/write_listing_to_index/find_or_create_listing.rb
@@ -2,10 +2,6 @@ class WriteListingToIndex
   class FindOrCreateListing
     include Interactor
 
-    def call
-      context.listing = IronBase::Listing.find(listing_id) || IronBase::Listing.new(id: listing_id)
-    end
-
     def rollback
       if context.listing.persisted?
         if should_destroy?
@@ -16,20 +12,58 @@ class WriteListingToIndex
       end
     end
 
+    def call
+      context.listing = IronBase::Listing.find(listing_id) || IronBase::Listing.new(id: listing_id)
+      context.listing.url = {
+          purchase: purchase_url,
+          page: page_url
+      }
+    end
+
     def listing_id
-      Digest::MD5.hexdigest(id_tagged_url || id_base_url)
+      Digest::MD5.hexdigest(
+          "#{base_url}#{context.listing_json.id}"
+      )
     end
 
-    def id_tagged_url
-      return unless context.listing_json.id
-      "#{id_base_url}!#{context.listing_json.id}"
+    def purchase_url
+      if affiliate_link_tag
+        "#{base_url}#{affiliate_link_tag}"
+      elsif share_a_sale?
+        share_a_sale_url
+      else
+        base_url
+      end
     end
 
-    def id_base_url
-      context.listing_json.url ||
-          context.page.redirect_from ||
-          context.url.purchase
+    def share_a_sale_url
+      link = base_url.split(/https?\:\/\//).last
+      link = link.to_query('urllink')
+      link = link.gsub(".","%2E").gsub("-","%2D")
+      "http://www.shareasale.com/r.cfm?u=882338&b=358708&m=37742&afftrack=&#{link}"
     end
+
+    def base_url
+      context.listing_json.url || page_url
+    end
+
+    def page_url
+      if context.page.code == 302    # Temporary redirect, so
+        context.page.redirect_from   # preserve original url
+      else
+        context.page.url
+      end
+    end
+
+    def affiliate_link_tag
+      context.site.affiliate_link_tag
+    end
+
+    def share_a_sale?
+      context.site.affiliate_program == "ShareASale"
+    end
+
+    # Rollback status checkers
 
     def should_destroy?
       listing_is_duplicate? ||
--- spec/interactors/find_or_create_listing_spec.rb
+++ spec/interactors/find_or_create_listing_spec.rb
@@ -1,6 +1,29 @@
 require 'spec_helper'
 
 describe WriteListingToIndex::FindOrCreateListing do
+  before :each do
+    @site = create_site "www.retailer.com"
+    @listing_json = Hashie::Mash.new(
+        "valid"               => true,
+        "condition"           =>"new",
+        "type"                =>"RetailListing",
+        "availability"        =>"in_stock",
+        "location"            =>"1900 East Warner Ave. Ste., 1-D, Santa Ana, CA 92705",
+        "title"               => "CITADEL 1911 COMPACT .45ACP 3 1/2\" HOGUE BLACK",
+        "keywords"            => "CITADEL 1911 COMPACT .45ACP",
+        "image"               => "http://www.emf-company.com/store/pc/catalog/1911CITCSPHBat10MED.JPG",
+        "price_in_cents"      => "65000",
+        "sale_price_in_cents" => "65000",
+        "description"         => ".45ACP, 3 1/2\" BARREL, HOGUE BLACK GRIPS",
+        "product_category1"   => "Guns",
+        "product_sku"         => "1911-CIT45CSPHB"
+    )
+    @page = Hashie::Mash.new(
+        url: "http://#{@site.domain}/1",
+        code: 200
+    )
+  end
+
   describe '#call' do
     it 'retrieves a listing object for a listing that already exists in the index' do
       listing = create(:listing)
@@ -10,11 +33,14 @@ describe WriteListingToIndex::FindOrCreateListing do
           url: listing.url.page
       )
       IronBase::Listing.refresh_index
+
       listing2 = WriteListingToIndex::FindOrCreateListing.call(
           listing_json: listing_json,
           page:         page,
-          url:          listing.url
+          url:          listing.url,
+          site:         @site
       ).listing
+
       expect(listing2.persisted?).to eq(true)
       expect(IronBase::Listing.count).to eq(1)
       expect(IronBase::Listing.find(listing2.id).id).to eq(listing.id)
@@ -29,52 +55,102 @@ describe WriteListingToIndex::FindOrCreateListing do
           url: 'http://www.retailer.com/1'
       )
       IronBase::Listing.refresh_index
+
       listing = WriteListingToIndex::FindOrCreateListing.call(
           listing_json: listing_json,
           url: Hashie::Mash.new(page: page.url, purchase: page.url),
-          page: page
+          page: page,
+          site: @site
       ).listing
+
       expect(listing.persisted?).to eq(false)
       expect(IronBase::Listing.count).to eq(0)
       expect(IronBase::Listing.find(listing.id)).to be_nil
     end
   end
 
+  describe '#purchase_url' do
+    it 'returns the untagged url for a site without a link tag' do
+      listing = WriteListingToIndex::FindOrCreateListing.call(
+          site:         @site,
+          page:         @page,
+          listing_json: @listing_json,
+      ).listing
+      expect(listing.url.purchase).to eq(@page.url)
+    end
+
+    it 'returns an affiliate url for ShareASale site' do
+      site = create_site 'www.botach.com'
+      url = 'http://www.botach.com/fnh-scar-17s-7-62mm-battle-rifles-tan/'
+      sas_link = "http://www.shareasale.com/r.cfm?u=882338&b=358708&m=37742&afftrack=&urllink=www%2Ebotach%2Ecom%2Ffnh%2Dscar%2D17s%2D7%2D62mm%2Dbattle%2Drifles%2Dtan%2F"
+
+      listing = WriteListingToIndex::FindOrCreateListing.call(
+          site:         site,
+          page:         @page.merge(url: "http://#{site.domain}/feed.xml"),
+          listing_json: @listing_json.merge(url: url),
+      ).listing
+      expect(listing.url.purchase).to eq(sas_link)
+    end
+
+    it 'returns the tagged url for a site with a link tag' do
+      site = Site.new(domain: 'www.luckygunner.com', source: :fixture)
+      site.send(:write_to_redis)
+      url = "http://#{site.domain}/product"
+      tagged_url = "#{url}#{site.affiliate_link_tag}"
+
+      listing = WriteListingToIndex::FindOrCreateListing.call(
+          site:         site,
+          page:         @page.merge(url: url),
+          listing_json: @listing_json,
+      ).listing
+      expect(listing.url.purchase).to eq(tagged_url)
+    end
+  end
+
   describe '#listing_id' do
     it 'should use the purchase url when there is no id tag' do
-      listing_json = Hashie::Mash.new(title: 'New Listing')
       page_url = 'http://www.retailer.com'
       purchase_url = "#{page_url}-buy"
+      listing_json = Hashie::Mash.new(
+          title: 'New Listing',
+          url: purchase_url
+      )
       page = Hashie::Mash.new(
           code: 200,
           url: page_url
       )
+
       listing = WriteListingToIndex::FindOrCreateListing.call(
           listing_json: listing_json,
-          url: Hashie::Mash.new(page: page_url, purchase: purchase_url),
-          page: page
+          page: page,
+          site: @site
       ).listing
+
       expect(listing.id).to eq(Digest::MD5.hexdigest(purchase_url))
     end
 
     it 'should use an id tagged url when there is an id tag in the listing json' do
       id_tag = '-123'
-      listing_json = Hashie::Mash.new(
-          title: 'New Listing',
-          id: id_tag
-      )
       page_url = 'http://www.retailer.com'
       purchase_url = "#{page_url}-buy"
+      listing_json = Hashie::Mash.new(
+          title: 'New Listing',
+          id: id_tag,
+          url: purchase_url
+      )
+
       page = Hashie::Mash.new(
           code: 200,
           url: page_url
       )
+
       listing = WriteListingToIndex::FindOrCreateListing.call(
           listing_json: listing_json,
-          url: Hashie::Mash.new(page: page_url, purchase: purchase_url),
-          page: page
+          page: page,
+          site: @site
       ).listing
-      expect(listing.id).to eq(Digest::MD5.hexdigest("#{purchase_url}!#{id_tag}"))
+
+      expect(listing.id).to eq(Digest::MD5.hexdigest("#{purchase_url}#{id_tag}"))
     end
   end
 end
