b95e95ac1be20540ed7d9d946d3cf1166a528abf
Jon Stokes <jon@jonstokes.com>
2014-12-11 14:28:46 -0600

    Let loadables mutate context hash directly

--- app/interactors/write_listing_to_index/run_loadable_scripts.rb
+++ app/interactors/write_listing_to_index/run_loadable_scripts.rb
@@ -18,17 +18,8 @@ class WriteListingToIndex
     def call
       context.site.loadables.each do |script_name|
         runner = Loadable::Script.runner(script_name)
-        instance = Hashie::Mash.new(
-            listing:      context.listing,
-            listing_json: context.listing_json,
-            product:      context.product,
-            message1:     context.message1,
-            message2:     context.message2,
-            message3:     context.message3,
-            message4:     context.message4
-        )
         runner.attributes.each do |attribute_name, value|
-          value.call(instance)
+          value.call(context)
         end
       end
     end
