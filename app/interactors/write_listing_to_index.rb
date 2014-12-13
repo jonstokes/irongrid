455e0a68621b010dd72339450be53d3bd64aa3b1
Jon Stokes <jon@jonstokes.com>
2014-12-12 22:02:58 -0600

    Removed ironsights-specific interactors

--- app/interactors/write_listing_to_index.rb
+++ app/interactors/write_listing_to_index.rb
@@ -8,7 +8,6 @@ class WriteListingToIndex
     MergeJsonIntoListing,
     UpdateAndLinkProduct,
     RunLoadableScripts,
-    IronsightsListingDetails,
     SetListingDigest,
     LinkDenormalizedLocation,
     #UpdateListingImage,
