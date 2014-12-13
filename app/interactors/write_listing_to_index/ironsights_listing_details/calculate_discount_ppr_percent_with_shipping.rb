ebcc7a72cec9f0e5e465ed46f3e89ab0ea74b26f
Jon Stokes <jon@jonstokes.com>
2014-12-12 19:13:40 -0600

    Fix for discount with shipping

--- app/interactors/write_listing_to_index/ironsights_listing_details/calculate_discount_ppr_percent_with_shipping.rb
+++ app/interactors/write_listing_to_index/ironsights_listing_details/calculate_discount_ppr_percent_with_shipping.rb
@@ -6,7 +6,7 @@ class WriteListingToIndex
 
       def call
         return unless shipping_included? && discounted? && should_calculate_ppr?
-        listing.discount.ppr_percent =
+        listing.with_shipping.discount.ppr_percent =
             calculate_discount_percent(list_price_per_round, with_shipping.price.per_round)
       end
     end
