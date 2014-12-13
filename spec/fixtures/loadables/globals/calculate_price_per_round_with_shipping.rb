8cc0f3a07cb99b079c87cf21a03f08c1a7cc5331
Jon Stokes <jon@jonstokes.com>
2014-12-12 20:06:19 -0600

    Fixed a fixture

--- spec/fixtures/loadables/globals/calculate_price_per_round_with_shipping.rb
+++ spec/fixtures/loadables/globals/calculate_price_per_round_with_shipping.rb
@@ -1,15 +1,8 @@
-class WriteListingToIndex
-  class IronsightsListingDetails
-    class CalculatePricePerRoundWithShipping
-      include Interactor
-      include Ironsights::ListingCalculations
-
-      def call
-        return unless shipping_included? && should_calculate_ppr?
-
-        with_shipping.price.per_round =
-            calculate_price_per_round(with_shipping.price.current)
-      end
+Loadable::Script.define do
+  script "ironsights/globals/calculate_price_per_round_with_shipping" do
+    price_per_round_with_shipping do
+      next unless shipping_included? && should_calculate_ppr?
+      calculate_price_per_round(with_shipping.price.current)
     end
   end
 end
