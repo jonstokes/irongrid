9b5db58794d333790013fa20d609b2f21dd9841f
Jon Stokes <jon@jonstokes.com>
2015-01-15 12:05:24 -0600

    Set ES logging to nil in prod

--- config/initializers/iron_base.rb
+++ config/initializers/iron_base.rb
@@ -3,7 +3,7 @@ IronBase::Settings.configure do |config|
   config.redis_url             = Figaro.env.ironsights_redis_url
   config.elasticsearch_url     = Figaro.env.elasticsearch_url_remote
   config.elasticsearch_index   = Figaro.env.index_name
-  config.logger                = Rails.logger
+  config.logger                = Rails.env.production? ? nil : Rails.logger
   config.aws_access_key_id     = Figaro.env.aws_access_key_id
   config.aws_secret_access_key = Figaro.env.aws_secret_access_key
   config.aws_bucket_name       = 'irongrid-backup-production'
