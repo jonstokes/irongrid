9ff582f8e30b15cf939c49188cf44da0f8c6e00b
Jon Stokes <jon@jonstokes.com>
2015-02-04 17:36:50 -0600

    Pool size tweak

--- config/initializers/iron_base.rb
+++ config/initializers/iron_base.rb
@@ -3,7 +3,7 @@ IronBase::Settings.configure do |config|
   config.redis_url             = Figaro.env.ironsights_redis_url
   config.elasticsearch_url     = Figaro.env.elasticsearch_url_remote
   config.elasticsearch_index   = Figaro.env.index_name
-  config.elasticsearch_pool_size = Figaro.env.elasticsearch_pool_size rescue 25
+  config.elasticsearch_pool_size = Figaro.env.elasticsearch_pool_size.to_i rescue 25
   config.logger                = Rails.env.production? ? nil : Rails.logger
   config.aws_access_key_id     = Figaro.env.aws_access_key_id
   config.aws_secret_access_key = Figaro.env.aws_secret_access_key
