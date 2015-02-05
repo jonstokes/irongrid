4bafab436a6498152e0604ced02e7a33707ac9d6
Jon Stokes <jon@jonstokes.com>
2015-02-04 19:14:11 -0600

    More logging

--- app/services/read_listings_service.rb
+++ app/services/read_listings_service.rb
@@ -4,6 +4,8 @@ class ReadListingsService < CoreService
 
   def each_job
     Site.each do |site|
+      notify "Checking site #{site.domain}"
+      notify "Should add job for #{site.domain}: #{should_add_job?(site)} [lq any: #{site.listings_queue.any?}, PLW empty: #{PullListingsWorker.jobs_in_flight_with_domain(site.domain).empty?0}]"
       next unless should_add_job?(site)
       yield(klass: "PullListingsWorker", arguments: {domain: site.domain})
     end
