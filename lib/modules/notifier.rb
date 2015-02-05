840263946d851c510ebc218d0c9afa7a4559734e
Jon Stokes <jon@jonstokes.com>
2015-02-04 18:49:20 -0600

    Notifier logs out to stdout and to loggly

--- lib/modules/notifier.rb
+++ lib/modules/notifier.rb
@@ -6,6 +6,7 @@ module Notifier
   def notify(logline, opts={})
     domain_insert = @domain ? "[#{@domain}]": ""
     error_insert = (opts[:type] == :error) ? "##ERROR## " : ""
-    Rails.logger.info "[#{self.class}](#{Thread.current.object_id})#{domain_insert}: #{error_insert}#{logline}"
+    complete_logline = "[#{self.class}](#{Thread.current.object_id})#{domain_insert}: #{error_insert}#{logline}"
+    $log.info complete_logline
   end
 end
