1adb2f2ed30991224ff637d6b741d452ddeb1e73
Jon Stokes <jon@jonstokes.com>
2015-02-06 16:05:51 -0600

    Simplify sidekiq clearing

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,6 +1,3 @@
 def clear_sidekiq
-  Sidekiq::Worker.clear_all
-  %w(db_fast_high db_fast_low db_slow_high db_slow_low crawls crawl_images stretched).each do |q|
-    Sidekiq::Queue.new(q).clear
-  end
+  Sidekiq.redis { |c| c.flushdb }
 end
