6a44eef6473ee6979531922a6162ef343dd8a20c
Jon Stokes <jon@jonstokes.com>
2015-02-09 12:40:44 -0600

    Use threadsafe cache for connection pools

--- config/initializers/bellbro.rb
+++ config/initializers/bellbro.rb
@@ -3,28 +3,21 @@ def pool_size
   size.zero? ? 10 : size
 end
 
-stretched = ConnectionPool.new(size: pool_size) do
+connection_hash = ThreadSafe::Cache.new
+connection_hash[:stretched_redis_pool] = ConnectionPool.new(size: pool_size) do
   Redis.new(url: Figaro.env.stretched_redis_pool)
 end
-
-irongrid = ConnectionPool.new(size: pool_size) do
+connection_hash[:irongrid_redis_pool] = ConnectionPool.new(size: pool_size) do
   Redis.new(url: Figaro.env.irongrid_redis_pool)
 end
-
-validator = ConnectionPool.new(size: pool_size) do
+connection_hash[:validator_redis_pool] = ConnectionPool.new(size: pool_size) do
   Redis.new(url: Figaro.env.validator_redis_pool)
 end
-
-ironsights = ConnectionPool.new(size: pool_size) do
+connection_hash[:ironsights_redis_pool] = ConnectionPool.new(size: pool_size) do
   Redis.new(url: Figaro.env.ironsights_redis_pool)
 end
 
 Bellbro::Settings.configure do |config|
   config.logger = Rails.logger
-  config.connection_pools = {
-      irongrid_redis_pool:   irongrid,
-      validator_redis_pool:  validator,
-      ironsights_redis_pool: ironsights,
-      stretched_redis_pool:  stretched
-  }
+  config.connection_pools = connection_hash
 end
