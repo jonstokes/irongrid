653942d363a5df4d0bd036342d80650613d6c316
Jon Stokes <jon@jonstokes.com>
2015-02-10 16:02:14 -0600

    More fixes

--- config/initializers/bellbro.rb
+++ config/initializers/bellbro.rb
@@ -2,9 +2,10 @@ config_hash = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 connection_hash = ThreadSafe::Cache.new
 
 config_hash.each do |pool, config|
-  puts "## Initializing redis pool #{pool} with size #{config['pool']} and url #{config['url']}"
+  url = Figaro.env.send(config['url'])
+  puts "## Initializing redis pool #{pool} with size #{config['pool']} and url #{url}"
   connection_hash[pool.to_sym] = ConnectionPool.new(size: config['pool']) do
-    Redis.new(url: config['url'])
+    Redis.new(url: url)
   end
 end
 
