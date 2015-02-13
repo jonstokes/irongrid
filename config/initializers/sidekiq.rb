9ecd742e0853e1b974572f0bf742bef76ae2203d
Jon Stokes <jon@jonstokes.com>
2015-02-13 15:14:01 -0600

    Don't namespace redis at all

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ end
 
 Sidekiq.configure_server do |config|
   config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
-  config.redis = { :url => sidekiq_redis_url, :namespace => Rails.env }
+  config.redis = { :url => sidekiq_redis_url }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
   end
@@ -16,7 +16,7 @@ end
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
   config.error_handlers << Proc.new { |ex,context| Airbrake.notify_or_ignore(ex,parameters: context) }
-  config.redis = { :url => sidekiq_redis_url, :namespace => Rails.env }
+  config.redis = { :url => sidekiq_redis_url }
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::Client::Batch
   end
