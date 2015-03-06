b1c23b1427f4ea3933a0499020624f537375dd12
Jon Stokes <jon@jonstokes.com>
2015-03-06 12:55:23 -0600

    Fixes

--- app/services/delete_listings_for_full_feeds_service.rb
+++ app/services/delete_listings_for_full_feeds_service.rb
@@ -1,13 +1,28 @@
 class DeleteListingsForFullFeedsService < BaseService
-  track_with_schema jobs_started: Integer
+  track_with_schema(
+      jobs_started: Integer,
+      listings_deleted: Integer
+  )
   poll_interval 3600
   worker_class DeleteListingsWorker
+
+  def start_jobs
+    each_job do |job, domain|
+      jid = worker_class.perform_async(job)
+      ring "Starting job #{jid} #{worker_class.name} for #{domain} with #{job.inspect}."
+      record_incr(:jobs_started)
+      ring "Job #{jid} will delete #{record[:data][:listings_deleted]} for #{domain}"
+      status_update(true)
+      record[:data][:listings_deleted] = 0
+    end
+  end
 
   def each_job
     IronCore::Site.each_full_product_feed_site do |site|
       next unless should_add_job?(site)
       IronBase::Listing.find_each(query_hash(site)) do |batch|
-        yield(batch.map(&:id))
+        record[:data][:listings_deleted] += batch.size
+        yield batch.map(&:id), site.domain
       end
     end
   end
