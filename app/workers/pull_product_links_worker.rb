4adf1d0f13e29cfd13e5e103b4f50cbc089ef8cc
Jon Stokes <jon@jonstokes.com>
2015-06-06 13:12:55 -0500

    Fix for schema for pull prod links worker

--- app/workers/pull_product_links_worker.rb
+++ app/workers/pull_product_links_worker.rb
@@ -3,9 +3,7 @@ class PullProductLinksWorker < BaseWorker
 
   track_with_schema(
     objects_deleted: Integer,
-    links_created:   Integer,
-    transition:      String,
-    next_jid:        String
+    links_created:   Integer
   )
 
   before :should_run?, :track
@@ -16,10 +14,13 @@ class PullProductLinksWorker < BaseWorker
     while !timed_out? && obj = site.product_links_queue.pop
       record_incr(:objects_deleted)
       next unless obj.object.try(:product_link)
-      record_incr(:links_created)
-      site.link_message_queue.push IronCore::LinkMessage.new(url: obj.object.product_link)
+      record_incr(:links_created) unless push_link(obj.object.product_link).zero?
     end
     log "Pulled #{record[:data][:links_created]} product links for #{site.domain}. Queue size is #{site.product_links_queue.size}"
+  end
+
+  def push_link(link)
+    site.link_message_queue.push IronCore::LinkMessage.new(url: link)
   end
 
   def self.should_run?(site)
